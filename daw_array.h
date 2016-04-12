ff54cd840d4bfa22f642d183619a3b63564da82e
Darrell Wright <dwright-git@dawdevel.ca>
2016-04-11 22:59:21 -0400

    Added swap/copy/move to array

--- daw_array.h
+++ daw_array.h
@@ -23,6 +23,16 @@
 #pragma once
 
 namespace daw {
+	template<typename T> struct array;
+	
+	template<typename T>
+	void swap( array<T> & first, array<T> & second ) nothrow {
+        using std::swap;
+		swap( first.m_begin, second.m_begin );
+		swap( first.m_end, second.m_end );
+		swap( first.m_size, second.m_size );
+    }
+
 	template<typename T>
 	struct array {
 		using value_type = T;
@@ -36,11 +46,47 @@ namespace daw {
 		size_t m_size;
 
 	public:
-		array( size_t count ): m_begin( new value_type[count] ), m_end( m_begin + count ), m_size( count ) { }
+		friend void swap( array & first, array & second ) nothrow;
 
-		array( size_t count, value_type def_value ): m_begin( new value_type[count] ), m_end( m_begin + count ), m_size( count ) {
+		array( ): m_begin( nullptr ), m_end( nullptr ), m_size( 0 ) { }
+
+		array( size_t count ): m_begin( nullptr ), m_end( nullptr ), m_size( count ) {
+			m_begin = new value_type[count];
+			m_end = m_begin + count;
+		}
+
+		array( size_t count, value_type def_value ): m_begin( nullptr ), m_end( nullptr ), m_size( count ) {
+			m_begin = new value_type[count];
+			m_end = m_begin + count;
+
 			for( auto it = m_begin; it != m_end; ++it ) {
 				*it = def_value;
+			}
+		}
+
+		array( array && ) = default;
+		array & operator=( array && ) = default;
+
+		arary( array const & other ):  m_begin( nullptr ), m_end( nullptr ), m_size( other.m_size ) {
+			m_begin = new value_type[count];
+			m_end = m_begin + count;
+			std::copy( m_begin, m_end, other.m_begin );
+		}
+
+		array & operator=( array rhs ) {
+			if( this != &rhs ) {
+				swap( *this, rhs );	
+			}
+			return *this;
+		}
+
+		~array( ) {
+			if( m_begin ) {
+				auto tmp = m_begin;
+				m_begin = nullptr;
+				m_end = nullptr;
+				count = 0;
+				delete[] tmp;
 			}
 		}
 
@@ -48,6 +94,10 @@ namespace daw {
 			return m_size;
 		}
 
+		bool empty( ) const {
+			return 0 == m_size;
+		}
+	
 		iterator begin( ) {
 			return m_begin;
 		}
@@ -86,6 +136,25 @@ namespace daw {
 			return *(m_begin + pos);
 		}
 
+		reference front( ) {
+			return *m_begin;
+		}
 
+		const_reference front( ) const {
+			return *m_begin;
+		}
+
+		reference back( ) {
+			auto tmp = m_end;
+			--tmp;
+			return *tmp;
+		}
+
+		const_reference back( ) const {
+			auto tmp = m_end;
+			--tmp;
+			return *tmp;
+		}
 	};	// struct array
 }	// namespace daw
+
