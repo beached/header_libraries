2205682b3be53b49ec779a10a784eb60bce8c3b2
Darrell Wright <dwright-git@dawdevel.ca>
2016-09-03 01:00:19 -0400

    updated daw_cstring.h

--- daw_cstring.h
+++ daw_cstring.h
@@ -23,6 +23,8 @@
 #pragma once
 
 #include <string>
+#include <cstdint>
+
 #include <daw/scope_guard.h>
 #include <daw/daw_traits.h>
 #include <daw/daw_newhelper.h>
@@ -30,98 +32,110 @@
 namespace daw {
 	namespace data {
 		template<typename CharType = char>
-		class CString {
-		public:
-			CString( const CharType* c_str, bool do_copy = false, const size_t length = 0 ) :m_cstr( c_str ), m_local_string( do_copy ) {
-				if( do_copy ) {
-					size_t len = length;
-					if( 0 == len ) {
-						len = strlen( c_str );
+			struct CString {
+				CharType const* m_cstr;
+				bool m_local_string;
+			public:
+				CString( CharType const* c_str, bool do_copy = false, const size_t length = 0 ) :m_cstr( c_str ), m_local_string( do_copy ) {
+					if( do_copy ) {
+						size_t len = length;
+						if( 0 == len ) {
+							len = strlen( c_str );
+						}
+						CharType* tmp = nullptr;
+						SCOPE_EXIT {
+							m_cstr = tmp;
+						};
+						tmp = new_array_throw<CharType>( len + 1 );
+						memcpy( tmp, c_str, len + 1 );
 					}
-					CharType* tmp = nullptr;
-					SCOPE_EXIT {
-						m_cstr = tmp;
-					};
-					tmp = new_array_throw<CharType>( len + 1 );
-					memcpy( tmp, c_str, len + 1 );
 				}
-			}
 
-			CString( ) noexcept:m_cstr( nullptr ), m_local_string( true ) { }
-			CString( const CString& value ) = delete;
-			CString& operator=(const CString& rhs) = delete;
+				CString( ) noexcept:
+					m_cstr{ nullptr },
+					m_local_string{ true } { }
 
-			CString( CString&& value ) noexcept: m_cstr( std::move( value.m_cstr ) ), m_local_string( std::move( value.m_local_string ) ) {
-				value.nullify( );
-			}
+				CString( const CString& value ) = delete;
+				CString& operator=(const CString& rhs) = delete;
 
-			CString& operator=(CString&& rhs) {
-				if( this != &rhs ) {
-					m_cstr = std::move( rhs.m_cstr );
-					m_local_string = std::move( rhs.m_local_string );
-					rhs.nullify( );
+				CString( CString&& value ) noexcept: 
+					m_cstr{ std::exchange( value.m_cstr, nullptr ) },
+					m_local_string{ std::exchange( value.m_local_string, false ) } { }
+
+				void swap( CString & rhs ) noexcept {
+					using std::swap;
+					swap( m_cstr, rhs.m_cstr );
+					swap( m_local_string, rhs.m_local_string );
 				}
-				return *this;
-			}
 
-			~CString( ) noexcept {
-				if( m_local_string && nullptr != m_cstr ) {
-					delete[] m_cstr;
-					nullify( );
+				CString& operator=(CString&& rhs) noexcept {
+					if( this != &rhs ) {
+						CString tmp{ std::move( rhs ) };
+						tmp.swap( *this );
+					}
+					return *this;
 				}
-			}
 
-			bool operator==(const CString& rhs) const = delete;
+				~CString( ) noexcept {
+					if( m_local_string && nullptr != m_cstr ) {
+						auto tmp = m_cstr;
+						nullify( );
+						delete[] tmp;
+					}
+				}
 
-			const CharType& operator[]( size_t pos ) const {
-				return m_cstr[pos];
-			}
+				bool operator==(const CString& rhs) const = delete;
 
-			void nullify( ) noexcept {
-				m_cstr = nullptr;
-				m_local_string = false;
-			}
+				CharType const& operator[]( size_t pos ) const {
+					return m_cstr[pos];
+				}
+
+				void nullify( ) noexcept {
+					m_cstr = nullptr;
+					m_local_string = false;
+				}
 
 				const char* move( ) noexcept {
-				auto result = m_cstr;
-				nullify( );
-				return result;
-			}
+					auto result = m_cstr;
+					nullify( );
+					return result;
+				}
 
 				const char* get( ) noexcept {
-				return m_cstr;
-			}
+					return m_cstr;
+				}
 
 				std::string to_string( ) const {
-				return std::string( m_cstr );
-			}
-
-			size_t size( ) const noexcept {
-				if( is_null( ) ) {
-					return 0;
+					return std::string( m_cstr );
 				}
-				return strlen( m_cstr );
-			}
+
+				size_t size( ) const noexcept {
+					if( is_null( ) ) {
+						return 0;
+					}
+					return strlen( m_cstr );
+				}
 
 				bool is_null( ) const noexcept {
-				return nullptr == m_cstr;
-			}
+					return nullptr == m_cstr;
+				}
 
 				bool is_local_string( ) const noexcept {
-				return m_local_string;
-			}
+					return m_local_string;
+				}
 
 				void take_ownership_of_data( ) noexcept {
-				m_local_string = true;
-			}
+					m_local_string = true;
+				}
+			};	// CString
 
-		private:
-			const CharType* m_cstr;
-			bool m_local_string;
-		};
+		template<typename... Args>
+			void swap( CString<Args...> & lhs, CString<Args...> & rhs ) noexcept {
+				lhs.swap( rhs );
+			}
 
 		// TODO		static_assert(daw::traits::is_regular<CString<char>>::value, "CString isn't regular");
 
-		using cstring = CString < char > ;
+		using cstring = CString<char> ;
 	}	// namespace data
 }	// namespace daw
