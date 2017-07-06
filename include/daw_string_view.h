d3901b077ecd3f2d133a88a3460a60f8ef3e9a04
Darrell Wright <dwright-git@dawdevel.ca>
2017-07-05 23:04:02 -0400

    .

--- include/daw_string_view.h
+++ include/daw_string_view.h
@@ -106,7 +106,7 @@ namespace daw {
 		using iterator = const_iterator;
 		using reverse_iterator = std::reverse_iterator<iterator>;
 		using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-		using size_type_internal = uint32_t; //std::size_t;
+		using size_type_internal = std::size_t;
 		using size_type = std::size_t;
 		using difference_type = std::ptrdiff_t;
 
