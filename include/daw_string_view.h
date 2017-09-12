7c4f05155a2ff2523f5275a202cd45a427c9a210
Darrell Wright <dwright-git@dawdevel.ca>
2017-09-12 12:53:31 -0400

    .

--- include/daw_string_view.h
+++ include/daw_string_view.h
@@ -164,7 +164,8 @@ namespace daw {
 		size_type_internal m_size;
 
 	  public:
-		static constexpr size_type_internal const npos = std::numeric_limits<size_type_internal>::max( );
+		static constexpr size_type_internal const npos;
+		//= std::numeric_limits<size_type_internal>::max( );
 
 		constexpr basic_string_view( ) noexcept : m_first{nullptr}, m_size{0} {}
 
@@ -626,7 +627,7 @@ namespace daw {
 		}
 	}; // basic_string_view
 	template<typename CharT, typename Traits>
-	constexpr size_type_internal const basic_string_view::npos = std::numeric_limits<size_type_internal>::max( );
+	constexpr basic_string_virw<CharT, Traits>::size_type_internal const basic_string_view<CharT, Traits>::npos = std::numeric_limits<basic_string_view<CharT, Traits>::size_type_internal>::max( );
 
 	using string_view = basic_string_view<char>;
 	using wstring_view = basic_string_view<wchar_t>;
