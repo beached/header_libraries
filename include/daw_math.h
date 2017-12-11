915583889f5f6af82600f04368b1f4becb978744
Darrell Wright <dwright-git@dawdevel.ca>
2017-12-11 11:54:28 -0500

    Added vmin/vmax

--- include/daw_math.h
+++ include/daw_math.h
@@ -229,5 +229,32 @@ namespace daw {
 #pragma clang diagnostic pop
 #endif
 
+		template<typename T>
+		T vmin( T &&t ) {
+			return std::forward<T>( t );
+		}
+
+		template<typename T0, typename T1, typename... Ts>
+		typename std::common_type<T0, T1, Ts...>::type vmin( T0 &&val1, T1 &&val2, Ts &&... vs ) {
+			if( val2 < val1 ) {
+				return vmin( val2, std::forward<Ts>( vs )... );
+			} else {
+				return vmin( val1, std::forward<Ts>( vs )... );
+			}
+		}
+
+		template<typename T>
+		T vmax( T &&t ) {
+			return std::forward<T>( t );
+		}
+
+		template<typename T0, typename T1, typename... Ts>
+		typename std::common_type<T0, T1, Ts...>::type vmax( T0 &&val1, T1 &&val2, Ts &&... vs ) {
+			if( val2 > val1 ) {
+				return vmax( val2, std::forward<Ts>( vs )... );
+			} else {
+				return vmax( val1, std::forward<Ts>( vs )... );
+			}
+		}
 	} // namespace math
 } // namespace daw
