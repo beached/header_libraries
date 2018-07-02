05c737254538b8f7ead4d641cb1d538ee358514d
Darrell Wright <dwright-git@dawdevel.ca>
2018-07-01 21:50:49 -0400

    missing header

--- include/daw/daw_traits.h
+++ include/daw/daw_traits.h
@@ -24,6 +24,7 @@
 
 #include <cstdint>
 #include <deque>
+#include <iterator>
 #include <list>
 #include <map>
 #include <ostream>
@@ -868,7 +869,7 @@ namespace daw {
 
 		template<typename Container, size_t ExpectedSize>
 		constexpr bool is_value_size_equal_v =
-		  sizeof( *std::begin( std::declval<Container>( ) ) ) == ExpectedSize;
+		  sizeof( *std::cbegin( std::declval<Container>( ) ) ) == ExpectedSize;
 
 		namespace impl {
 			template<typename... Ts>
