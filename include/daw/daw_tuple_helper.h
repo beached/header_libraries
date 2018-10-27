ea1db6e41d7d5f3e6e51ecf711b427a76e048921
Darrell Wright <dwright-git@dawdevel.ca>
2018-10-27 14:37:56 -0400

    Added make_from_tuple

--- include/daw/daw_tuple_helper.h
+++ include/daw/daw_tuple_helper.h
@@ -28,6 +28,7 @@
 #include <tuple>
 #include <utility>
 
+#include "cpp_17.h"
 #include "daw_math.h"
 #include "daw_static_array.h"
 
@@ -388,4 +389,24 @@ namespace daw {
 			  std::index_sequence_for<Args...>{} );
 		}
 	} // namespace tuple
+
+	namespace detail {
+		template<class T, class Tuple, std::size_t... I>
+		constexpr T make_from_tuple_impl2( Tuple &&t, std::index_sequence<I...>, std::true_type ) {
+			return T{std::get<I>( std::forward<Tuple>( t ) )...};
+		}
+
+		template<class T, class Tuple, std::size_t... I>
+		constexpr T make_from_tuple_impl2( Tuple &&t, std::index_sequence<I...>, std::false_type ) {
+			return T(std::get<I>( std::forward<Tuple>( t ) )...);
+		}
+	} // namespace detail
+
+	template<class T, class Tuple>
+	constexpr T make_from_tuple2( Tuple &&t ) {
+		return detail::make_from_tuple_impl2<T>(
+		  std::forward<Tuple>( t ),
+		  std::make_index_sequence<
+		    std::tuple_size_v<std::remove_reference_t<Tuple>>>{}, std::is_aggregate<T>{} );
+	}
 } // namespace daw
