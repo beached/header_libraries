9ba0e48ea1d67d6c1939fe8d2682d188a69c6220
Darrell Wright <dwright-git@dawdevel.ca>
2018-12-23 20:06:09 -0500

    .

--- tests/daw_static_hash_map_test.cpp
+++ tests/daw_static_hash_map_test.cpp
@@ -23,20 +23,22 @@
 #include "daw/daw_static_hash_map.h"
 #include "daw/daw_string_view.h"
 
-static constexpr daw::static_hash_map<uint16_t, daw::string_view, 13> const
-  status_codes{{{100, "Continue"},
-                {101, "Switching Protocols"},
-                {102, "Processing"},
-                {200, "OK"},
-                {201, "Created"},
-                {202, "Accepted"},
-                {203, "Non-Authoritative Information"},
-                {204, "No Content"},
-                {205, "Reset Content"},
-                {206, "Partial Content"},
-                {207, "Multi-Status"},
-                {208, "Already Reported"},
-                {226, "IM Used"}}};
+namespace {
+	constexpr daw::static_hash_map<uint16_t, daw::string_view, 13> const
+	  status_codes{{{100, "Continue"},
+	                {101, "Switching Protocols"},
+	                {102, "Processing"},
+	                {200, "OK"},
+	                {201, "Created"},
+	                {202, "Accepted"},
+	                {203, "Non-Authoritative Information"},
+	                {204, "No Content"},
+	                {205, "Reset Content"},
+	                {206, "Partial Content"},
+	                {207, "Multi-Status"},
+	                {208, "Already Reported"},
+	                {226, "IM Used"}}};
+} // namespace
 
 int main( ) {
 	return static_cast<int>( status_codes[204].size( ) );
