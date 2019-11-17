f13229484214e178c6f4962376d03513e8c86954
Darrell Wright <dwright-git@dawdevel.ca>
2019-11-17 04:08:14 -0500

    .

--- ci_test.sh
+++ ci_test.sh
@@ -2,7 +2,7 @@
 
 mkdir build
 cd build
-CC=clang-9 CXX=clang++-9 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug ..
+CC=clang CXX=clang++ CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . --config Debug --target full -j 2
 ctest -C Debug
 
