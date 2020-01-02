fdcd31bca2b871ee0611b6ab083f6f288dfdf1c5
Darrell Wright <dwright-git@dawdevel.ca>
2020-01-01 23:27:48 -0500

    bad brace

--- include/daw/daw_do_not_optimize.h
+++ include/daw/daw_do_not_optimize.h
@@ -3,14 +3,14 @@
 // Copyright (c) 2020 Darrell Wright
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files( the "Software" ), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-// copies of the Software, and to permit persons to whom the Software is
+// of this software and associated documentation files( the "Software" ), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and / or
+// sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -61,10 +61,8 @@ namespace daw {
 	}
 #else
 	namespace internal {
-		namespace {
-			[[maybe_unused]] constexpr void UseCharPointer( char const volatile * ) {}
-		} // namespace
-	}   // namespace internal
+		[[maybe_unused]] constexpr void UseCharPointer( char const volatile * ) {}
+	} // namespace internal
 
 	template<class T>
 	inline void do_not_optimize( T const &value ) {
@@ -72,7 +70,5 @@ namespace daw {
 		  &reinterpret_cast<char const volatile &>( value ) );
 		_ReadWriteBarrier( );
 	}
-}
 #endif
-}
-
+} // namespace daw
