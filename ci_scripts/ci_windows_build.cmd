e20b8a5cd8c9e709067b41c6ab0686764bc7eb3e
Darrell Wright <dwright-git@dawdevel.ca>
2020-01-12 12:33:57 -0500

    updated win ci script

--- ci_scripts/ci_windows_build.cmd
+++ ci_scripts/ci_windows_build.cmd
@@ -2,6 +2,8 @@
 choco install ninja
 md build
 cd build
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\cvarsall.bat" x64
+#getting dir
+set
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\cvars64.bat"
 cmake -DCMAKE_BUILD_TYPE=Debug -GNinja ..
 cmake --build . --config Debug --target full -j 2 -- -k 1000
