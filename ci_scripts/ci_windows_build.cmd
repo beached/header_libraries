56ad5aeda178ed69dde030badb0cd0b0fd51181b
Darrell Wright <dwright-git@dawdevel.ca>
2020-01-12 14:43:38 -0500

    specifying c/cxx compilers for win ci script

--- ci_scripts/ci_windows_build.cmd
+++ ci_scripts/ci_windows_build.cmd
@@ -1,9 +1,17 @@
+REM ##############################
+REM Installing Ninja
+choco install ninja
 md build
 cd build
 
 REM ##############################
+REM Setting VCVars
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+REM ##############################
 REM Running cmake
-cmake ..
+set CMAKE_CXX_COMPILER=cl.exe
+set CMAKE_C_COMPILER=cl.exe
+cmake -DCMAKE_BUILD_TYPE=Debug -GNinja ..
 REM ##############################
 REM Building
-cmake --build . --config Debug --target full -j 2
+cmake --build . --config Debug --target full -j 2 -- -k 1000
