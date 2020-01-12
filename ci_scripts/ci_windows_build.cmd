50471447ec8e56b87e9410999872c4c184c73a4f
Darrell Wright <dwright-git@dawdevel.ca>
2020-01-12 12:48:48 -0500

    undoing win ci script

--- ci_scripts/ci_windows_build.cmd
+++ ci_scripts/ci_windows_build.cmd
@@ -1,15 +1,9 @@
-REM ##############################
-REM Installing Ninja
-choco install ninja
 md build
 cd build
 
 REM ##############################
-REM Setting VCVars
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
-REM ##############################
 REM Running cmake
-cmake -DCMAKE_BUILD_TYPE=Debug -GNinja ..
+cmake ..
 REM ##############################
 REM Building
-cmake --build . --config Debug --target full -j 2 -- -k 1000
+cmake --build . --config Debug --target full -j 2
