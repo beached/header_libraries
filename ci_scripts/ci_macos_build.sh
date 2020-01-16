8b536a8e892007fa29b63603b934ac25f6b02fae
Darrell Wright <dwright-git@dawdevel.ca>
2020-01-15 22:11:36 -0500

    fixed config part of scripts

--- ci_scripts/ci_macos_build.sh
+++ ci_scripts/ci_macos_build.sh
@@ -10,3 +10,5 @@ cmake -DCMAKE_BUILD_TYPE=Debug ..
 echo "#################################"
 echo "Starting full build"
 cmake --build . --config Debug --target full -j 2 -- -k 1000
+echo "#################################"
+echo "Full build complete"
--- ci_scripts/ci_ubuntu_build.sh
+++ ci_scripts/ci_ubuntu_build.sh
@@ -10,4 +10,6 @@ CC=clang-9 CXX=clang++-9 CXXFLAGS=-stdlib=libc++ cmake -DCMAKE_BUILD_TYPE=Debug 
 echo "#################################"
 echo "Starting full build"
 cmake --build . --config Debug --target full -j 2 -- -k 1000
+echo "#################################"
+echo "Full build complete"
 
