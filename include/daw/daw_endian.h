1ec13406c8491363b9fcaedf427737e484144254
Darrell Wright <dwright-git@dawdevel.ca>
2020-01-15 21:34:31 -0500

    removed all dependencies.

--- include/daw/daw_endian.h
+++ include/daw/daw_endian.h
@@ -22,150 +22,139 @@
 
 #pragma once
 
+#include "cpp_17.h"
+
 #include <cstddef>
 #include <cstdint>
 #include <type_traits>
 #include <utility>
 
-#include "cpp_17.h"
-
 namespace daw {
-	struct endian {
+	enum class endian {
 #ifdef _WIN32
-		static constexpr int const little = 0;
-		static constexpr int const big = 1;
-		static constexpr int const native = little;
+		little = 1234,
+		big = 4321,
+		native = little
 #else
-		static constexpr int const little = __ORDER_LITTLE_ENDIAN__;
-		static constexpr int const big = __ORDER_BIG_ENDIAN__;
-		static constexpr int const native = __BYTE_ORDER__;
+		little = __ORDER_LITTLE_ENDIAN__,
+		big = __ORDER_BIG_ENDIAN__,
+		native = __BYTE_ORDER__
 #endif
-		static_assert( little != big, "Both little and big endian are set" );
 	};
 
-	namespace impl {
+	namespace endian_details {
 		template<typename T>
-		constexpr decltype( auto )
-		swap_bytes( T &&value,
+		constexpr T
+		swap_bytes( T value,
 		            std::integral_constant<size_t, sizeof( uint8_t )> ) noexcept {
-			return std::forward<T>( value );
+			return value;
 		}
 
 		template<typename T>
-		constexpr auto
-		swap_bytes( T &&value,
+		constexpr T
+		swap_bytes( T value,
 		            std::integral_constant<size_t, sizeof( uint16_t )> ) noexcept {
-			return static_cast<daw::remove_cvref_t<T>>(
+			return static_cast<T>(
 			  static_cast<uint16_t>(
-			    ( static_cast<uint16_t>( value ) >> static_cast<uint16_t>( 8 ) ) &
-			    static_cast<uint16_t>( 0x00FF ) ) |
+			    ( static_cast<uint16_t>( value ) >> static_cast<uint16_t>( 8U ) ) &
+			    static_cast<uint16_t>( 0x00FFU ) ) |
 			  static_cast<uint16_t>(
-			    ( static_cast<uint16_t>( value ) << static_cast<uint16_t>( 8 ) ) &
-			    static_cast<uint16_t>( 0xFF00 ) ) );
+			    ( static_cast<uint16_t>( value ) << static_cast<uint16_t>( 8U ) ) &
+			    static_cast<uint16_t>( 0xFF00U ) ) );
 		}
 
 		template<typename T>
-		constexpr auto
-		swap_bytes( T &&value,
+		constexpr T
+		swap_bytes( T value,
 		            std::integral_constant<size_t, sizeof( uint32_t )> ) noexcept {
-			return static_cast<daw::remove_cvref_t<T>>(
+			return static_cast<T>(
 			  static_cast<uint32_t>(
-			    ( static_cast<uint32_t>( value ) >> static_cast<uint32_t>( 24 ) ) &
-			    static_cast<uint32_t>( 0x0000'00FF ) ) |
+			    ( static_cast<uint32_t>( value ) >> static_cast<uint32_t>( 24U ) ) &
+			    static_cast<uint32_t>( 0x0000'00FFU ) ) |
 			  static_cast<uint32_t>(
-			    ( static_cast<uint32_t>( value ) >> static_cast<uint32_t>( 8 ) ) &
-			    static_cast<uint32_t>( 0x0000'FF00 ) ) |
+			    ( static_cast<uint32_t>( value ) >> static_cast<uint32_t>( 8U ) ) &
+			    static_cast<uint32_t>( 0x0000'FF00U ) ) |
 			  static_cast<uint32_t>(
-			    ( static_cast<uint32_t>( value ) << static_cast<uint32_t>( 8 ) ) &
-			    static_cast<uint32_t>( 0x00FF'0000 ) ) |
+			    ( static_cast<uint32_t>( value ) << static_cast<uint32_t>( 8U ) ) &
+			    static_cast<uint32_t>( 0x00FF'0000U ) ) |
 			  static_cast<uint32_t>(
-			    ( static_cast<uint32_t>( value ) << static_cast<uint32_t>( 24 ) ) &
-			    static_cast<uint32_t>( 0xFF00'0000 ) ) );
+			    ( static_cast<uint32_t>( value ) << static_cast<uint32_t>( 24U ) ) &
+			    static_cast<uint32_t>( 0xFF00'0000U ) ) );
 		}
 
 		template<typename T>
-		constexpr auto
-		swap_bytes( T &&value,
+		constexpr T
+		swap_bytes( T value,
 		            std::integral_constant<size_t, sizeof( uint64_t )> ) noexcept {
-			return static_cast<daw::remove_cvref_t<T>>(
+			return static_cast<T>(
 			  static_cast<uint64_t>(
-			    ( static_cast<uint64_t>( value ) >> static_cast<uint64_t>( 56 ) ) &
-			    static_cast<uint64_t>( 0x0000'0000'0000'00FF ) ) |
+			    ( static_cast<uint64_t>( value ) >> static_cast<uint64_t>( 56U ) ) &
+			    static_cast<uint64_t>( 0x0000'0000'0000'00FFU ) ) |
 			  static_cast<uint64_t>(
-			    ( static_cast<uint64_t>( value ) >> static_cast<uint64_t>( 40 ) ) &
-			    static_cast<uint64_t>( 0x0000'0000'0000'FF00 ) ) |
+			    ( static_cast<uint64_t>( value ) >> static_cast<uint64_t>( 40U ) ) &
+			    static_cast<uint64_t>( 0x0000'0000'0000'FF00U ) ) |
 			  static_cast<uint64_t>(
-			    ( static_cast<uint64_t>( value ) >> static_cast<uint64_t>( 24 ) ) &
-			    static_cast<uint64_t>( 0x0000'0000'00FF'0000 ) ) |
+			    ( static_cast<uint64_t>( value ) >> static_cast<uint64_t>( 24U ) ) &
+			    static_cast<uint64_t>( 0x0000'0000'00FF'0000U ) ) |
 			  static_cast<uint64_t>(
-			    ( static_cast<uint64_t>( value ) >> static_cast<uint64_t>( 8 ) ) &
-			    static_cast<uint64_t>( 0x0000'0000'FF00'0000 ) ) |
+			    ( static_cast<uint64_t>( value ) >> static_cast<uint64_t>( 8U ) ) &
+			    static_cast<uint64_t>( 0x0000'0000'FF00'0000U ) ) |
 			  static_cast<uint64_t>(
 			    ( static_cast<uint64_t>( value ) << static_cast<uint64_t>( 8U ) ) &
-			    static_cast<uint64_t>( 0x0000'00FF'0000'0000 ) ) |
+			    static_cast<uint64_t>( 0x0000'00FF'0000'0000U ) ) |
 			  static_cast<uint64_t>(
-			    ( static_cast<uint64_t>( value ) << static_cast<uint64_t>( 24 ) ) &
-			    static_cast<uint64_t>( 0x0000'FF00'0000'0000 ) ) |
+			    ( static_cast<uint64_t>( value ) << static_cast<uint64_t>( 24U ) ) &
+			    static_cast<uint64_t>( 0x0000'FF00'0000'0000U ) ) |
 			  static_cast<uint64_t>(
-			    ( static_cast<uint64_t>( value ) << static_cast<uint64_t>( 40 ) ) &
-			    static_cast<uint64_t>( 0x00FF'0000'0000'0000 ) ) |
+			    ( static_cast<uint64_t>( value ) << static_cast<uint64_t>( 40U ) ) &
+			    static_cast<uint64_t>( 0x00FF'0000'0000'0000U ) ) |
 			  static_cast<uint64_t>(
-			    ( static_cast<uint64_t>( value ) << static_cast<uint64_t>( 56 ) ) &
-			    static_cast<uint64_t>( 0xFF00'0000'0000'0000 ) ) );
+			    ( static_cast<uint64_t>( value ) << static_cast<uint64_t>( 56U ) ) &
+			    static_cast<uint64_t>( 0xFF00'0000'0000'0000U ) ) );
 		}
-	} // namespace impl
+	} // namespace endian_details
 
 	template<typename T,
-	         std::enable_if_t<(endian::native == endian::little and
-	                           std::is_integral_v<daw::remove_cvref_t<T>>),
-	                          std::nullptr_t> = nullptr>
-	constexpr decltype( auto ) to_little_endian( T &&value ) noexcept {
-		return std::forward<T>( value );
-	}
-
-	template<typename T>
-	constexpr auto to_little_endian( T &&value ) noexcept
-	  -> std::enable_if_t<(endian::native != endian::little and
-	                       std::is_integral_v<daw::remove_cvref_t<T>>),
-	                      decltype( value )> {
-
-		return impl::swap_bytes( std::forward<T>( value ),
-		                         std::integral_constant<size_t, sizeof( T )>{} );
-	}
-
-	template<typename T>
-	constexpr auto to_big_endian( T &&value ) noexcept
-	  -> std::enable_if_t<(endian::native == endian::big and
-	                       std::is_integral_v<daw::remove_cvref_t<T>>),
-	                      decltype( value )> {
-		return std::forward<T>( value );
+	         std::enable_if_t<std::is_integral_v<T>, std::nullptr_t> = nullptr>
+	constexpr T to_little_endian( T value ) noexcept {
+		if constexpr( endian::native == endian::little ) {
+			return value;
+		} else {
+			return endian_details::swap_bytes(
+			  value, std::integral_constant<size_t, sizeof( T )>{} );
+		}
 	}
 
 	template<typename T,
-	         std::enable_if_t<(endian::native != endian::big and
-	                           std::is_integral_v<daw::remove_cvref_t<T>>),
-	                          std::nullptr_t> = nullptr>
-	constexpr auto to_big_endian( T &&value ) noexcept {
-		return impl::swap_bytes( std::forward<T>( value ),
-		                         std::integral_constant<size_t, sizeof( T )>{} );
+	         std::enable_if_t<std::is_integral_v<T>, std::nullptr_t> = nullptr>
+	constexpr T to_big_endian( T value ) noexcept {
+		if constexpr( endian::native == endian::big ) {
+			return value;
+		} else {
+			return endian_details::swap_bytes(
+			  value, std::integral_constant<size_t, sizeof( T )>{} );
+		}
 	}
 
-	template<typename T>
-	constexpr decltype( auto ) host_to_network_endian( T &&value ) noexcept {
-		return to_big_endian( std::forward<T>( value ) );
+	template<typename T,
+	         std::enable_if_t<std::is_integral_v<T>, std::nullptr_t> = nullptr>
+	constexpr T network_to_host_endian( T value ) noexcept {
+		if constexpr( endian::native == endian::big ) {
+			return value;
+		} else {
+			return to_little_endian( value );
+		}
 	}
 
-	template<typename T>
-	constexpr auto network_to_host_endian( T &&value ) noexcept
-	  -> std::enable_if_t<( endian::native == endian::big ), decltype( value )> {
-
-		return std::forward<T>( value );
-	}
-
-	template<typename T>
-	constexpr auto network_to_host_endian( T &&value ) noexcept
-	  -> std::enable_if_t<( endian::native != endian::big ), T> {
-
-		return to_little_endian( std::forward<T>( value ) );
+	template<endian SourceEndian, typename T,
+	         std::enable_if_t<std::is_integral_v<T>, std::nullptr_t> = nullptr>
+	constexpr T to_native_endian( T value ) noexcept {
+		if constexpr( SourceEndian == endian::native ) {
+			return value;
+		} else if constexpr( endian::native == endian::little ) {
+			return to_little_endian( value );
+		} else {
+			return to_big_endian( value );
+		}
 	}
 } // namespace daw
