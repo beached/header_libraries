9fe5a8b92b3b1928e32b9d1d574723e42fb73123
Darrell Wright <beached@users.noreply.github.com>
2020-06-15 16:10:56 -0400

    Updated overload to handle function pointers and lifting

--- tests/daw_overload_test.cpp
+++ tests/daw_overload_test.cpp
@@ -88,8 +88,24 @@ void daw_empty_overload_001( ) {
 	// daw::expecting( can_a2 );
 }
 
+namespace daw_lift_overload_001 {
+	int add( int a, int b ) {
+		return a + b;
+	}
+	int add2( int a ) {
+		return a;
+	}
+
+	void test( ) {
+		auto add_set = daw::overload( daw::lift<add>, daw::lift<add2> );
+		static_assert( sizeof( decltype( add_set ) ) == 1 );
+		daw::expecting( add_set( 2, 4 ) == 6 );
+		daw::expecting( add_set( 4 ) == 4 );
+	}
+} // namespace daw_lift_overload_001
 int main( ) {
 	daw_overload_001( );
 	daw_overload_002( );
 	daw_empty_overload_001( );
+	daw_lift_overload_001::test( );
 }
