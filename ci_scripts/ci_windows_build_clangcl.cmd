664324fbccd1e7bcc7f4d0df00e9f73362880d4d
Darrell Wright <beached@users.noreply.github.com>
2020-07-18 17:33:15 -0400

    added llvm chocolately install

--- ci_scripts/ci_windows_build_clangcl.cmd
+++ ci_scripts/ci_windows_build_clangcl.cmd
@@ -4,6 +4,7 @@ ECHO "##############################"
 ECHO "Installing Ninja"
 vcpkg upgrade
 vcpkg install ninja
+choco install llvm
 
 md build
 cd build
@@ -14,7 +15,7 @@ ECHO "Setting VCVars"
 
 ECHO "##############################"
 ECHO "Running cmake"
-cmake -DCMAKE_BUILD_TYPE=Debug -GNinja ..
+cmake -DCMAKE_BUILD_TYPE=Debug -GNinja -DCMAKE_CXX_COMPILER=clang-cl.exe -DCMAKE_C_COMPILER=clang-cl.exe ..
 
 ECHO "##############################"
 ECHO "Building"
