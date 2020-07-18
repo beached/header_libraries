eab42e76879560271c30ae7eff783b8dc95ba210
Darrell Wright <beached@users.noreply.github.com>
2020-07-18 18:14:41 -0400

    Added explicit cast

--- tests/daw_memory_mapped_file_test.cpp
+++ tests/daw_memory_mapped_file_test.cpp
@@ -37,7 +37,7 @@ void create_file( String &&str ) {
 
 void daw_memory_mapped_file_001( std::string const &file_name ) {
 	create_file( file_name );
-	daw::filesystem::memory_mapped_file_t<std::uint8_t> test( file_name );
+	daw::filesystem::memory_mapped_file_t<std::uint8_t> test( static_cast<std::string_view>( file_name ) );
 }
 
 int main( ) {
