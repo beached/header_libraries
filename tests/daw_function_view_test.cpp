b1d75536aaacd9724354040596c959074df63268
Darrell Wright <beached@users.noreply.github.com>
2020-12-12 02:30:27 -0500

    fixed formatting

--- tests/daw_function_view_test.cpp
+++ tests/daw_function_view_test.cpp
@@ -10,25 +10,29 @@
 
 #include <cstdio>
 
-int func(daw::function_view<int(int, int, int)> f) {
+int func( daw::function_view<int( int, int, int )> f ) {
 	return f( 1, 2, 3 ) * f( 4, 5, 7 );
 }
 
-int test() {
-  return func([](int a, int b, int c) { return a * b * c; });
+int test( ) {
+	return func( []( int a, int b, int c ) { return a * b * c; } );
 }
 
-inline constexpr int add(int a, int b, int c) { return a + b + c; }
+inline constexpr int add( int a, int b, int c ) {
+	return a + b + c;
+}
 
-int test2() { return func(add); }
+int test2( ) {
+	return func( add );
+}
 
-int func2(daw::function_view<void(double, double)> f ) {
+int func2( daw::function_view<void( double, double )> f ) {
 	f( 1.2, 3.4 );
 	return 0;
 }
 
-int test3() {
-  return func2(+[](double a, double b) { puts("Hello\n"); });
+int test3( ) {
+	return func2( +[]( double a, double b ) { puts( "Hello\n" ); } );
 }
 
-int main( ) { }
+int main( ) {}
