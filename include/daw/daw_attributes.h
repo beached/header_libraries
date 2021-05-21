632fe861c087d7d9cd18e785daacd601fa98adff
Darrell Wright <beached@users.noreply.github.com>
2021-05-20 23:44:32 -0400

    Added DAW_NO_FLATTEN flag check

--- include/daw/daw_attributes.h
+++ include/daw/daw_attributes.h
@@ -12,16 +12,36 @@
 
 #if defined( __GNUC__ ) or defined( __clang__ )
 
+#if not defined( DAW_NO_FLATTEN )
+
 #define DAW_ATTRIB_FLATTEN [[gnu::flatten]]
 #define DAW_ATTRIB_FLATINLINE [[gnu::flatten, gnu::always_inline]]
+
+#else
+
+#define DAW_ATTRIB_FLATTEN 
+#define DAW_ATTRIB_FLATINLINE
+
+#endif
+
 #define DAW_ATTRIB_HIDDEN __attribute__( ( visibility( "hidden" ) ) )
 #define DAW_ATTRIB_INLINE [[gnu::always_inline]]
 #define DAW_ATTRIB_NOINLINE [[gnu::noinline]]
 
 #elif defined( _MSC_VER )
 
+#if not defined( DAW_NO_FLATTEN )
+
 #define DAW_ATTRIB_FLATTEN
 #define DAW_ATTRIB_FLATINLINE __forceinline
+
+#else
+
+#define DAW_ATTRIB_FLATTEN 
+#define DAW_ATTRIB_FLATINLINE
+
+#endif
+
 #define DAW_ATTRIB_HIDDEN
 #define DAW_ATTRIB_INLINE __forceinline
 #define DAW_ATTRIB_NOINLINE __declspec( noinline )
