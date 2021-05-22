5a2e5a7e439e790fa97c750f239121b3e6465cd9
Darrell Wright <beached@users.noreply.github.com>
2021-05-22 16:09:35 -0400

    Readded daw_hide.h for old code

---
+++ include/daw/daw_hide.h
@@ -0,0 +1,17 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/header_libraries
+//
+
+#pragma once
+
+#if !defined( DAW_NO_FLATTEN ) and !defined( _MSC_VER )
+#define DAW_ATTRIBUTE_FLATTEN [[gnu::flatten, gnu::always_inline]]
+#define DAW_ATTRIBUTE_HIDDEN __attribute__( ( visibility( "hidden" ) ) )
+#else
+#define DAW_ATTRIBUTE_FLATTEN
+#define DAW_ATTRIBUTE_HIDDEN
+#endif
