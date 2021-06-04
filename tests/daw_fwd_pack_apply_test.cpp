b3f91d75c4796521d6c7a2de61f6f79becb0ea3c
Darrell Wright <beached@users.noreply.github.com>
2021-06-03 20:20:13 -0400

    Added daw_fwd_pack apply that allows apply to a forwarding only tuple

---
+++ tests/daw_fwd_pack_apply_test.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/header_libraries
+//
+
+#include <daw/daw_fwd_pack_apply.h>
+
+struct Add {
+	constexpr int operator( )( int a, int b ) const noexcept {
+		return a + b;
+	}
+};
+
+static_assert( daw::apply( Add{}, daw::fwd_pack{ 1, 2 } ) == 3 );
+
+
+int main( ) { }
+
