9ad9445a0179ed58cff43e27b1f8507806e8b58e
Darrell Wright <beached@users.noreply.github.com>
2021-10-14 03:45:50 -0400

    Added tuple2
    Tuple2 is a non-recursive/aggregate tuple implementation

---
+++ tests/daw_tuple2_test.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/header_libraries
+//
+
+#include <daw/daw_tuple2.h>
+
+int main( int argc, char ** ) {
+	auto a = daw::tuple2{ 1, 2.f, 3.0, 6 };
+	auto b = daw::tuple2{ '4', "5", argc };
+
+	auto big = daw::tuple2_cat(
+	  a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a,
+	  b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b,
+	  a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a,
+	  b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b, a, b );
+
+	return daw::get<6>( big ) == argc ? 0 : 1;
+}
