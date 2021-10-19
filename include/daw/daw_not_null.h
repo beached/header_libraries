90e93848f4e9c965dff3ad6e9f6e28d970032880
Darrell Wright <beached@users.noreply.github.com>
2021-10-19 01:24:13 -0400

    Forgot to add header for daw_not_null into git

---
+++ include/daw/daw_not_null.h
@@ -0,0 +1,55 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/
+//
+
+#pragma once
+
+#include "daw_assume.h"
+#include "daw_attributes.h"
+
+#include <ciso646>
+#include <iterator>
+#include <type_traits>
+
+namespace daw {
+	template<typename Pointer>
+	struct not_null {
+		static_assert( std::is_pointer_v<Pointer>, "Only pointers are supported" );
+
+		using pointer = Pointer;
+		using reference = typename std::iterator_traits<pointer>::reference;
+
+	private:
+		pointer m_ptr;
+
+	public:
+		DAW_ATTRIB_INLINE constexpr not_null( pointer p ) noexcept: m_ptr( p ) {
+			if( not p ) {
+				std::terminate( );
+			}
+		}
+
+		DAW_ATTRIB_INLINE constexpr reference operator*( ) const noexcept {
+			DAW_ASSUME( m_ptr != nullm_ptr );
+			return *m_ptr;
+		}
+
+		DAW_ATTRIB_INLINE constexpr pointer operator->( ) const noexcept {
+			return m_ptr;
+		}
+
+		DAW_ATTRIB_INLINE constexpr pointer get( ) const noexcept {
+			return m_ptr;
+		}
+
+		DAW_ATTRIB_INLINE constexpr operator pointer( ) const noexcept {
+			return m_ptr;
+		}
+	};
+}
+
+
