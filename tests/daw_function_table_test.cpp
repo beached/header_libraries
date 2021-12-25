b8dbaac9b41b161e4a1b6d2d9c1c5797150ae2cb
Darrell Wright <beached@users.noreply.github.com>
2021-12-24 23:13:50 -0500

    changed from printf to cout

--- tests/daw_function_table_test.cpp
+++ tests/daw_function_table_test.cpp
@@ -136,5 +136,5 @@ int main( int argc, char **argv ) {
 		ptr = argv[1];
 	}
 	uintmax_t result = ftable( static_cast<size_t>( *ptr ), 0U, ptr );
-	printf( "%lu\n", result );
+	std::cout << result << '\n';
 }
