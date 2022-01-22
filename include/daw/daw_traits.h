0035605d2f8714c8705fca8e1c774e91ae2fb680
Darrell Wright <beached@users.noreply.github.com>
2022-01-22 00:37:45 -0500

    Moved has_to_string to traits_is_a_type

--- include/daw/daw_traits.h
+++ include/daw/daw_traits.h
@@ -385,16 +385,6 @@ namespace daw::traits {
 	inline constexpr bool is_not_array_array_v =
 	  not daw::is_detected_v<detectors::is_array_array, String>;
 
-	namespace traits_details {
-		template<typename T>
-		using has_to_string_test =
-		  decltype( std::string( std::declval<T>( ).to_string( ) ) );
-	} // namespace traits_details
-
-	template<typename T>
-	inline constexpr bool has_to_string_v =
-	  is_detected_v<traits_details::has_to_string_test, T>;
-
 	template<typename String>
 	inline constexpr bool is_string_view_like_v =
 	  is_container_like_v<String const> and has_integer_subscript_v<String const>
