b87a14723cdbc8561d150870f63aae46f33735bd
Darrell Wright <beached@users.noreply.github.com>
2023-07-19 23:27:21 -0400

    .

--- tests/daw_visit_test.cpp
+++ tests/daw_visit_test.cpp
@@ -146,10 +146,8 @@ namespace test_006 {
 			return T14{ };
 		case 15:
 			return T15{ };
-		case 16:
-			return T16{ };
 		}
-		DAW_UNREACHABLE( );
+		return T16{ };
 	}
 	constexpr int foofoo( std::variant<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9,
 	                                   T10, T11, T12, T13, T14, T15, T16>
