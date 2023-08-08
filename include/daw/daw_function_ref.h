a1c2e4c5af3c1f2d60193779c7cb3cd90dbf0487
Darrell Wright <beached@users.noreply.github.com>
2023-08-08 18:58:54 -0400

    Added function_ref for low cost type erased invokcables on interfaces

---
+++ include/daw/daw_function_ref.h
@@ -0,0 +1,89 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/header_libraries
+//
+
+#pragma once
+
+#include "daw_consteval.h"
+#include "daw_traits.h"
+
+#include <type_traits>
+
+namespace daw {
+	template<typename>
+	struct function_ref;
+
+	template<typename Result, typename... Args>
+	class function_ref<Result( Args... )> {
+		void const *m_data = nullptr;
+		fp_t<Result( void const *, Args... )> m_fp = nullptr;
+
+	public:
+		function_ref( ) = default;
+
+		DAW_CONSTEVAL function_ref( std::nullptr_t ) noexcept {}
+
+		template<typename T,
+		         std::enable_if_t<not std::is_same_v<function_ref, T> and
+		                            std::is_invocable_r_v<Result, T, Args...> and
+		                            not std::is_function_v<T>,
+		                          std::nullptr_t> = nullptr>
+		constexpr function_ref( T const &fn ) noexcept
+		  : m_data( static_cast<void const *>( std::addressof( fn ) ) )
+		  , m_fp( []( void const *d, Args... args ) -> Result {
+			  return ( *reinterpret_cast<T const *>( d ) )( DAW_FWD( args )... );
+		  } ) {}
+
+		template<typename T,
+		         std::enable_if_t<not std::is_same_v<function_ref, T> and
+		                            std::is_invocable_r_v<Result, T, Args...> and
+		                            std::is_function_v<T>,
+		                          std::nullptr_t> = nullptr>
+		inline function_ref( T const &fn ) noexcept
+		  : m_data( reinterpret_cast<void const *>( std::addressof( fn ) ) )
+		  , m_fp( []( void const *d, Args... args ) -> Result {
+			  return ( *reinterpret_cast<T const *>( d ) )( DAW_FWD( args )... );
+		  } ) {}
+
+		inline Result operator( )( Args... args ) const {
+			return m_fp( m_data, DAW_FWD( args )... );
+		}
+
+		explicit constexpr operator bool( ) const noexcept {
+			return m_fp != nullptr;
+		}
+
+		constexpr void reset( ) noexcept {
+			m_data = nullptr;
+			m_fp = nullptr;
+		}
+
+		constexpr void const *data( ) const noexcept {
+			return m_data;
+		}
+
+		friend constexpr bool operator==( function_ref<Result( Args... )> fn,
+		                                  std::nullptr_t ) noexcept {
+			return static_cast<bool>( fn );
+		}
+
+		friend constexpr bool
+		operator==( std::nullptr_t, function_ref<Result( Args... )> fn ) noexcept {
+			return static_cast<bool>( fn );
+		}
+
+		friend constexpr bool operator!=( function_ref<Result( Args... )> fn,
+		                                  std::nullptr_t ) noexcept {
+			return not static_cast<bool>( fn );
+		}
+
+		friend constexpr bool
+		operator!=( std::nullptr_t, function_ref<Result( Args... )> fn ) noexcept {
+			return not static_cast<bool>( fn );
+		}
+	};
+} // namespace daw
---
+++ tests/daw_function_ref_test.cpp
@@ -0,0 +1,47 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/header_libraries
+//
+
+#include <cstdio>
+#include <daw/daw_function_ref.h>
+
+int func( daw::function_ref<int( int, int, int )> f ) {
+	return f( 1, 2, 3 ) * f( 4, 5, 7 );
+}
+
+int test( ) {
+	return func( []( int a, int b, int c ) {
+		return a * b * c;
+	} );
+}
+
+inline constexpr int add( int a, int b, int c ) {
+	return a + b + c;
+}
+
+int test2( ) {
+	return func( add );
+}
+
+int func2( daw::function_ref<void( double, double )> f ) {
+	f( 1.2, 3.4 );
+	return 0;
+}
+
+int test3( ) {
+	return func2( +[]( double, double ) {
+		puts( "Hello\n" );
+	} );
+}
+
+int test4( ) {
+	return func2( []( double, double ) {
+		puts( "Hello\n" );
+	} );
+}
+
+int main( ) {}
