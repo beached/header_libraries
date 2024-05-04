baebc94ef0ad3a11d3d2f663cca98b3ac3c1add0
Darrell Wright <beached@users.noreply.github.com>
2024-05-04 12:17:55 -0400

    More fixes for ci install test on macos

--- ci_scripts/ci_macos_install_test.sh
+++ ci_scripts/ci_macos_install_test.sh
@@ -7,9 +7,9 @@ cd build
 echo "#################################"
 echo "Starting Install Tests"
 cmake --install .
-cd /tmp/header_libraries/cmake/test_project/
+cd ../cmake/test_project/
 mkdir build
 cd build
-cmake ..
+cmake .. -CMAKE_MODULE_PATH=/tmp/header_libraries/lib/cmake
 cmake --build .
 ctest -C Debug -VV
