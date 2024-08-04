39a509d062f726915360483ad4910b9b9f6c2912
Darrell Wright <beached@users.noreply.github.com>
2024-08-04 13:27:38 -0400

    Removed param again as it was one of the issues

--- ci_scripts/ci_ubuntu_test.sh
+++ ci_scripts/ci_ubuntu_test.sh
@@ -4,4 +4,4 @@ cd build
 
 echo "#################################3"
 echo "Starting Tests"
-ctest -C Debug --build-target daw-header-libraries_full -j 2 -- -k 100
+ctest -C Debug --build-target daw-header-libraries_full -j 2
