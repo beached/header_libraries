ed86691069ed4f5a76a78c5e19df3b41456ae376
Darrell Wright <beached@users.noreply.github.com>
2024-08-29 01:04:07 -0400

    Added projection support to Sort, Min, Max, and MinMax

--- include/daw/pipelines/algorithm.h
+++ include/daw/pipelines/algorithm.h
@@ -14,6 +14,7 @@
 #include "range.h"
 
 #include <algorithm>
+#include <functional>
 #include <iterator>
 #include <type_traits>
 
@@ -22,9 +23,11 @@ namespace daw::pipelines {
 	concept Sortable = Range<R> and RandomIterator<iterator_t<R>>;
 
 	namespace pimpl {
-		template<typename Compare>
+		template<typename Compare = std::less<>,
+		         typename Projection = std::identity>
 		struct Sort_t {
 			DAW_NO_UNIQUE_ADDRESS Compare m_compare{ };
+			DAW_NO_UNIQUE_ADDRESS Projection m_projection{ };
 
 			[[nodiscard]] DAW_CPP23_STATIC_CALL_OP constexpr auto
 			operator( )( auto &&compare ) DAW_CPP23_STATIC_CALL_OP_CONST {
@@ -33,66 +36,123 @@ namespace daw::pipelines {
 
 			[[nodiscard]] constexpr decltype( auto )
 			operator( )( Sortable auto &&r ) const {
-				std::sort( std::begin( r ), std::end( r ), m_compare );
+				std::sort( std::begin( r ),
+				           std::end( r ),
+				           [&]( auto const &lhs, auto const &rhs ) {
+					           return std::invoke( m_compare,
+					                               std::invoke( m_projection, lhs ),
+					                               std::invoke( m_projection, rhs ) );
+				           } );
 				return DAW_FWD( r );
 			}
 		};
-		Sort_t( ) -> Sort_t<std::less<>>;
-		template<typename Compare>
-		Sort_t( Compare ) -> Sort_t<Compare>;
+		Sort_t( ) -> Sort_t<std::less<>, std::identity>;
 
 		template<typename Compare>
+		Sort_t( Compare ) -> Sort_t<Compare, std::identity>;
+
+		template<typename Compare, typename Projection>
+		Sort_t( Compare, Projection ) -> Sort_t<Compare, Projection>;
+
+		template<typename Compare = std::less<>,
+		         typename Projection = std::identity>
 		struct Max_t {
 			DAW_NO_UNIQUE_ADDRESS Compare m_compare{ };
+			DAW_NO_UNIQUE_ADDRESS Projection m_projection{ };
 
-			[[nodiscard]] DAW_CPP23_STATIC_CALL_OP constexpr auto
-			operator( )( auto &&compare ) DAW_CPP23_STATIC_CALL_OP_CONST {
-				return Max_t{ DAW_FWD( compare ) };
+			template<typename C, typename P = std::identity>
+			requires( not Range<C> ) //
+			  [[nodiscard]] DAW_CPP23_STATIC_CALL_OP constexpr auto
+			  operator( )( C &&compare, P &&projection = Projection{ } )
+			    DAW_CPP23_STATIC_CALL_OP_CONST {
+				return Max_t{ DAW_FWD( compare ), DAW_FWD( projection ) };
 			}
 
 			[[nodiscard]] constexpr auto operator( )( Range auto &&r ) const {
-				return std::max_element( std::begin( r ), std::end( r ), m_compare );
+				return std::max_element( std::begin( r ),
+				                         std::end( r ),
+				                         [&]( auto const &lhs, auto const &rhs ) {
+					                         return std::invoke(
+					                           m_compare,
+					                           std::invoke( m_projection, lhs ),
+					                           std::invoke( m_projection, rhs ) );
+				                         } );
 			}
 		};
-		Max_t( ) -> Max_t<std::less<>>;
+		Max_t( ) -> Max_t<>;
+
 		template<typename Compare>
 		Max_t( Compare ) -> Max_t<Compare>;
 
-		template<typename Compare>
+		template<typename Compare, typename Projection>
+		Max_t( Compare, Projection ) -> Max_t<Compare, Projection>;
+
+		template<typename Compare = std::less<>,
+		         typename Projection = std::identity>
 		struct Min_t {
 			DAW_NO_UNIQUE_ADDRESS Compare m_compare{ };
+			DAW_NO_UNIQUE_ADDRESS Projection m_projection{ };
 
-			[[nodiscard]] DAW_CPP23_STATIC_CALL_OP constexpr auto
-			operator( )( auto &&compare ) DAW_CPP23_STATIC_CALL_OP_CONST {
-				return Min_t{ DAW_FWD( compare ) };
+			template<typename C, typename P = std::identity>
+			requires( not Range<C> ) //
+			  [[nodiscard]] DAW_CPP23_STATIC_CALL_OP constexpr auto
+			  operator( )( C &&compare, P &&projection = Projection{ } )
+			    DAW_CPP23_STATIC_CALL_OP_CONST {
+				return Min_t{ DAW_FWD( compare ), DAW_FWD( projection ) };
 			}
 
 			[[nodiscard]] constexpr auto operator( )( Range auto &&r ) const {
-				return std::min_element( std::begin( r ), std::end( r ), m_compare );
+				return std::min_element( std::begin( r ),
+				                         std::end( r ),
+				                         [&]( auto const &lhs, auto const &rhs ) {
+					                         return std::invoke(
+					                           m_compare,
+					                           std::invoke( m_projection, lhs ),
+					                           std::invoke( m_projection, rhs ) );
+				                         } );
 			}
 		};
-		Min_t( ) -> Min_t<std::less<>>;
+		Min_t( ) -> Min_t<>;
+
 		template<typename Compare>
 		Min_t( Compare ) -> Min_t<Compare>;
 
-		template<typename Compare>
-		struct MinMax_t {
-			DAW_NO_UNIQUE_ADDRESS Compare m_comapre{ };
+		template<typename Compare, typename Projection>
+		Min_t( Compare, Projection ) -> Min_t<Compare, Projection>;
 
-			[[nodiscard]] DAW_CPP23_STATIC_CALL_OP constexpr auto
-			operator( )( auto &&compare ) DAW_CPP23_STATIC_CALL_OP_CONST {
-				return MinMax_t{ DAW_FWD( compare ) };
+		template<typename Compare = std::less<>,
+		         typename Projection = std::identity>
+		struct MinMax_t {
+			DAW_NO_UNIQUE_ADDRESS Compare m_compare{ };
+			DAW_NO_UNIQUE_ADDRESS Projection m_projection{ };
+
+			template<typename C, typename P = std::identity>
+			requires( not Range<C> ) //
+			  [[nodiscard]] DAW_CPP23_STATIC_CALL_OP constexpr auto
+			  operator( )( C &&compare, P &&projection = Projection{ } )
+			    DAW_CPP23_STATIC_CALL_OP_CONST {
+				return MinMax_t{ DAW_FWD( compare ), DAW_FWD( projection ) };
 			}
 
 			[[nodiscard]] constexpr auto operator( )( Range auto &&r ) const {
-				auto result =
-				  std::minmax_element( std::begin( r ), std::end( r ), m_comapre );
+				auto result = std::minmax_element(
+				  std::begin( r ),
+				  std::end( r ),
+				  [&]( auto const &lhs, auto const &rhs ) {
+					  return std::invoke( m_compare,
+					                      std::invoke( m_projection, lhs ),
+					                      std::invoke( m_projection, rhs ) );
+				  } );
 				return std::tuple{ result.first, result.second };
 			}
 		};
-		MinMax_t( ) -> MinMax_t<std::less<>>;
+		MinMax_t( ) -> MinMax_t<>;
+
 		template<typename Compare>
 		MinMax_t( Compare ) -> MinMax_t<Compare>;
+
+		template<typename Compare, typename Projection>
+		MinMax_t( Compare, Projection ) -> MinMax_t<Compare, Projection>;
 	} // namespace pimpl
 
 	inline constexpr auto Sort = pimpl::Sort_t{ };
