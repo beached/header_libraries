3fa8b340a2913c8bb07beb1424beaba50dbce054
Darrell Wright <beached@users.noreply.github.com>
2024-09-07 21:18:01 -0400

    Simplify formatter implementation for daw::basic_string_view
    Refactored the formatter specialization for daw::basic_string_view to inherit from std::formatter. The new implementation leverages base class functionality, streamlining code and improving maintainability.

--- include/daw/daw_format_string_view.h
+++ include/daw/daw_format_string_view.h
@@ -15,21 +15,15 @@
 
 namespace std {
 	template<typename CharT, daw::string_view_bounds_type Bounds>
-	struct formatter<daw::basic_string_view<CharT, Bounds>, CharT> {
-		std::formatter<std::basic_string_view<CharT>> fmter{ };
-
-		explicit formatter( ) = default;
-
-		template<class ParseContext>
-		constexpr ParseContext::iterator parse( ParseContext &ctx ) {
-			return fmter.parse( ctx );
-		}
+	struct formatter<daw::sv2::basic_string_view<CharT, Bounds>, CharT>
+	  : std::formatter<std::basic_string_view<CharT>, CharT> {
+		using base = std::formatter<std::basic_string_view<CharT>, CharT>;
 
 		template<typename Ctx>
-		Ctx::iterator format( daw::basic_string_view<CharT, Bounds> sv,
-		                      Ctx &ctx ) const {
-			return fmter.format( std::basic_string_view<CharT>( sv.data( ), sv.size( ) ),
-			              ctx );
+		constexpr Ctx::iterator
+		format( daw::sv2::basic_string_view<CharT, Bounds> sv, Ctx &ctx ) const {
+			return base::format(
+			  std::basic_string_view<CharT>( sv.data( ), sv.size( ) ), ctx );
 		}
 	};
 } // namespace std
