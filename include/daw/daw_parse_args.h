de6a778bcd84878263b605f2c44546ca740dc69e
Darrell Wright <beached@users.noreply.github.com>
2024-12-18 12:16:41 -0500

    Refactor argument parser and add unit tests.

--- include/daw/daw_parse_args.h
+++ include/daw/daw_parse_args.h
@@ -9,26 +9,21 @@
 #pragma once
 
 #include "ciso646.h"
+#include "cpp_20.h"
 #include "daw_string_view.h"
 
 #include <optional>
-#include <string>
 #include <vector>
 
 namespace daw {
 	namespace parse_arg_details {
 		struct is_ws_t {
 			constexpr bool operator( )( char c ) const {
-				return c <= 0x20;
+				return c <= 0x20 and c > 0x0;
 			}
 		};
+
 		inline constexpr auto is_ws = is_ws_t{ };
-		struct is_eq_t {
-			constexpr bool operator( )( char c ) const {
-				return c == '=';
-			}
-		};
-		inline constexpr auto is_eq = is_eq_t{ };
 	} // namespace parse_arg_details
 
 	class Arguments;
@@ -41,16 +36,19 @@ namespace daw {
 		friend class ::daw::Arguments;
 
 		constexpr Argument( daw::string_view args ) {
-			if( args.size( ) >= 2 and nsc_and( args[0] == '-', args[1] == '-' ) ) {
+			if( args.starts_with( "--" ) ) {
 				// We are a named argument, starting with '--'
 				args.remove_prefix( 2 );
-				name = args.pop_front_until( parse_arg_details::is_eq );
+				name = args.pop_front_until( '=' );
 				if( args.empty( ) ) {
 					return;
 				}
-				args.remove_prefix( );
 			}
 			// We have an assigned value
+			if( args.starts_with( '"' ) and args.ends_with( '"' ) ) {
+				args.remove_prefix( 1 );
+				args.remove_suffix( 1 );
+			}
 			value = args;
 		}
 	};
@@ -67,50 +65,50 @@ namespace daw {
 		using iterator = typename values_type::iterator;
 		using const_iterator = typename values_type::const_iterator;
 		using size_type = std::size_t;
-		using differnce_type = std::ptrdiff_t;
+		using difference_type = std::ptrdiff_t;
 
-		inline Arguments( int argc, char **argv ) {
+		DAW_CPP20_CX_ALLOC Arguments( int argc, char **argv ) {
 			if( argc > 0 and argv[0] != nullptr ) {
 				m_prog_name = argv[0];
 			}
 			for( int n = 1; n < argc; ++n ) {
-				m_arguments.push_back( { argv[n] } );
+				m_arguments.push_back( Argument{ argv[n] } );
 			}
 		}
 
-		daw::string_view program_name( ) const {
+		DAW_CPP20_CX_ALLOC daw::string_view program_name( ) const {
 			return m_prog_name;
 		}
 
-		const_iterator begin( ) const {
+		DAW_CPP20_CX_ALLOC const_iterator begin( ) const {
 			return m_arguments.begin( );
 		}
 
-		const_iterator cbegin( ) const {
+		DAW_CPP20_CX_ALLOC const_iterator cbegin( ) const {
 			return m_arguments.cbegin( );
 		}
 
-		const_iterator end( ) const {
+		DAW_CPP20_CX_ALLOC const_iterator end( ) const {
 			return m_arguments.end( );
 		}
 
-		const_iterator cend( ) const {
+		DAW_CPP20_CX_ALLOC const_iterator cend( ) const {
 			return m_arguments.cend( );
 		}
 
-		size_type size( ) const {
+		DAW_CPP20_CX_ALLOC size_type size( ) const {
 			return m_arguments.size( );
 		}
 
-		bool empty( ) const {
+		DAW_CPP20_CX_ALLOC bool empty( ) const {
 			return m_arguments.empty( );
 		}
 
-		const_reference operator[]( std::size_t idx ) const {
+		DAW_CPP20_CX_ALLOC const_reference operator[]( std::size_t idx ) const {
 			return m_arguments[idx];
 		}
 
-		std::optional<size_type>
+		DAW_CPP20_CX_ALLOC std::optional<size_type>
 		find_argument_position( daw::string_view name ) const {
 			auto pos = std::find_if( begin( ), end( ), [name]( Argument const &arg ) {
 				return arg.name == name;
@@ -121,7 +119,7 @@ namespace daw {
 			return static_cast<size_type>( std::distance( begin( ), pos ) );
 		}
 
-		std::optional<size_type> first_named_argument( ) const {
+		DAW_CPP20_CX_ALLOC std::optional<size_type> first_named_argument( ) const {
 			auto pos = std::find_if( begin( ), end( ), []( Argument const &arg ) {
 				return not arg.name.empty( );
 			} );
@@ -131,7 +129,7 @@ namespace daw {
 			return static_cast<size_type>( std::distance( begin( ), pos ) );
 		}
 
-		bool has_named_argument( ) const {
+		DAW_CPP20_CX_ALLOC bool has_named_argument( ) const {
 			return static_cast<bool>( first_named_argument( ) );
 		}
 	};
