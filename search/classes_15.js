a7a3ef7073591156ff8c087d3cfd10bf322ef3de
beached <beached@users.noreply.github.com>
2025-03-14 23:39:47 +0000

    Deploying to gh-pages from @ beached/header_libraries@5c04bbd8d000281636b9b8a3521c68795622e872 🚀

--- search/classes_15.js
+++ search/classes_15.js
@@ -5,5 +5,6 @@ var searchData=
   ['zip_5fcontainer_2',['zip_container',['../structdaw_1_1zip__container.html',1,'daw']]],
   ['zip_5fiterator_3',['zip_iterator',['../structdaw_1_1pipelines_1_1zip__iterator.html',1,'daw::pipelines::zip_iterator&lt; Iterators &gt;'],['../structdaw_1_1zip__iterator.html',1,'daw::zip_iterator&lt; Iterators &gt;']]],
   ['zip_5fiterator_3c_20iterator_5ft_3c_20ranges_20_3e_2e_2e_2e_20_3e_4',['zip_iterator&lt; iterator_t&lt; Ranges &gt;... &gt;',['../structdaw_1_1pipelines_1_1zip__iterator.html',1,'daw::pipelines']]],
-  ['zip_5fview_5',['zip_view',['../structdaw_1_1pipelines_1_1zip__view.html',1,'daw::pipelines']]]
+  ['zip_5ft_5',['Zip_t',['../structdaw_1_1pipelines_1_1_zip__t.html',1,'daw::pipelines']]],
+  ['zip_5fview_6',['zip_view',['../structdaw_1_1pipelines_1_1zip__view.html',1,'daw::pipelines']]]
 ];
--- search/variables_14.js
+++ search/variables_14.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['zero_5fterminated_0',['zero_terminated',['../namespacedaw_1_1sv2.html#a092c450c9dd35b1e7c76c3fe22be1149',1,'daw::sv2']]]
+  ['zero_5fterminated_0',['zero_terminated',['../namespacedaw_1_1sv2.html#a092c450c9dd35b1e7c76c3fe22be1149',1,'daw::sv2']]],
+  ['zip_1',['Zip',['../namespacedaw_1_1pipelines.html#ab6410eb582e292e3244f142ba8df29d9',1,'daw::pipelines']]]
 ];
