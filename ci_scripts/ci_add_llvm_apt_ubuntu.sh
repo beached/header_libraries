907198de7aeac2b684b1c0338ae8bcc657a19cc6
Darrell Wright <beached@users.noreply.github.com>
2025-03-15 17:04:04 -0400

    Update CI to use Ubuntu 22.04 and LLVM jammy repository
    Migrated CI workflow from Ubuntu 20.04 to 22.04

--- ci_scripts/ci_add_llvm_apt_ubuntu.sh
+++ ci_scripts/ci_add_llvm_apt_ubuntu.sh
@@ -3,4 +3,4 @@
 echo "#################################"
 echo "Installing llvm"
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-apt-add-repository 'deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-9 main'
+apt-add-repository 'deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy main'
