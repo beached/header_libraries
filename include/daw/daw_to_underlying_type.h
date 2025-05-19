cb1e9c7da33d4656286bf3ede24e8d2b094463f9
Darrell Wright <beached@users.noreply.github.com>
2025-05-19 01:51:57 -0400

    Add utility to convert enums to their underlying type
    Introduce `to_underlying_type` in `daw_to_underlying_type.h`, allowing seamless conversion of enums to their underlying integer type. This simplifies operations requiring direct manipulation of enum values.

---
+++ include/daw/daw_to_underlying_type.h
@@ -0,0 +1,19 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/header_libraries
+//
+
+#pragma once
+
+#include <type_traits>
+
+namespace daw {
+	template<typename Enum,
+	         std::enable_if_t<std::is_enum_v<Enum>, std::nullptr_t> = nullptr>
+	constexpr auto to_underlying_type( Enum e ) {
+		return static_cast<std::underlying_type_t<Enum>>( e );
+	}
+} // namespace daw
