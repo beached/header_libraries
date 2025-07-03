ea8b432ae66c7695c43e19cf0603c401cd23abbe
Darrell Wright <beached@users.noreply.github.com>
2025-07-03 19:40:43 -0400

    Update CI for Windows to use Visual Studio 2022 toolset

--- ci_scripts/ci_windows_test.cmd
+++ ci_scripts/ci_windows_test.cmd
@@ -3,4 +3,4 @@ cd build
 
 ECHO "#################################3"
 ECHO "Starting Tests"
-"C:/PROGRAM FILES (X86)/MICROSOFT VISUAL STUDIO/2019/ENTERPRISE/COMMON7/IDE/COMMONEXTENSIONS/MICROSOFT/CMAKE/CMake/bin/ctest.exe" -C Debug --target daw-header-libraries_full -j 2 -VV --timeout 300
+"C:/PROGRAM FILES (X86)/MICROSOFT VISUAL STUDIO/2022/ENTERPRISE/COMMON7/IDE/COMMONEXTENSIONS/MICROSOFT/CMAKE/CMake/bin/ctest.exe" -C Debug --target daw-header-libraries_full -j 2 -VV --timeout 300
