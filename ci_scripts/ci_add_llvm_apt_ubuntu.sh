4b11ef7b7997af29a87274da26e8174b63bc645d
Darrell Wright <beached@users.noreply.github.com>
2025-07-05 18:05:25 -0400

    Remove redundant macOS CI scripts and modernize workflows with matrix strategy

--- ci_scripts/ci_add_llvm_apt_ubuntu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "#################################"
-echo "Installing llvm"
-wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
-apt-add-repository 'deb http://apt.llvm.org/noble/ llvm-toolchain-noble main'
-apt-add-repository 'deb http://apt.llvm.org/noble/ llvm-toolchain-noble-20 main'
--- ci_scripts/ci_macos_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd build
-
-echo "#################################"
-echo "Starting Tests"
-ctest -C Debug --build-target daw-header-libraries_full -j 2
