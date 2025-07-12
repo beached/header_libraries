5435f1231566f8c27f911d088489c274233ca5f4
Darrell Wright <beached@users.noreply.github.com>
2025-07-12 17:37:49 -0400

    refactored string_view find to work around unreachable code issue with MSVC

--- include/daw/daw_string_view.h
+++ include/daw/daw_string_view.h
@@ -1937,21 +1937,25 @@ namespace daw {
 						}
 						return static_cast<std::size_t>(
 						  reinterpret_cast<CharT const *>( r ) - first );
+					} else {
+						for( std::size_t n = pos; n < sz; ++n ) {
+							if( first[n] == c ) {
+								return n;
+							}
+						}
+						return npos;
 					}
 				}
+				else {
 #endif
-#if defined( DAW_HAS_MSVC )
-#pragma warning( push )
-#pragma warning( disable :4702 )
-#endif
-				for( std::size_t n = pos; n < sz; ++n ) {
-					if( first[n] == c ) {
-						return n;
+					for( std::size_t n = pos; n < sz; ++n ) {
+						if( first[n] == c ) {
+							return n;
+						}
 					}
+					return npos;
+#if defined( DAW_HAS_IF_CONSTEVAL_COMPAT )
 				}
-				return npos;
-#if defined( DAW_HAS_MSVC )
-#pragma warning( pop )
 #endif
 			}
 
