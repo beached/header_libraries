1d13edb0b047b6c664b702849d706f5451daee4d
Darrell Wright <beached@users.noreply.github.com>
2025-12-19 20:02:27 -0500

    Refactor pipeline includes, add `drop` pipeline, optimize `Reverse` for reverse iterators, and enhance test cases

---
+++ include/daw/pipelines/drop.h
@@ -0,0 +1,38 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/header_libraries
+//
+
+#pragma once
+
+#include "daw/daw_move.h"
+#include "daw/pipelines/range.h"
+
+namespace daw::pipelines {
+	template<typename Pred>
+	constexpr auto drop_while( Pred &&p ) {
+		return [pred = DAW_FWD( p )]( auto &&r ) {
+			auto f = std::begin( r );
+			auto l = std::end( r );
+			while( f != l and pred( *f ) ) {
+				++f;
+			}
+			return range_t{ std::move( f ), std::move( l ) };
+		};
+	}
+
+	template<typename Pred>
+	constexpr auto drop_until( Pred &&p ) {
+		return [pred = DAW_FWD( p )]( auto &&r ) {
+			auto f = std::begin( r );
+			auto l = std::end( r );
+			while( f != l and not pred( *f ) ) {
+				++f;
+			}
+			return range_t{ std::move( f ), std::move( l ) };
+		};
+	}
+} // namespace daw::pipelines
