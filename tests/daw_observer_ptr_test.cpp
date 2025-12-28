aae92e1fe33e475e12e4ba0518902cf21a3f690c
Darrell Wright <beached@users.noreply.github.com>
2025-12-28 00:45:56 -0500

    Remove redundant template argument in `observer_ptr` construction

--- tests/daw_observer_ptr_test.cpp
+++ tests/daw_observer_ptr_test.cpp
@@ -15,7 +15,7 @@ struct Base {
 struct Child : Base {};
 
 int main( int, char **argv ) {
-	auto p = daw::observer_ptr<char const *>( argv[0] );
+	auto p = daw::observer_ptr( argv[0] );
 	daw_ensure( p == argv[0] );
 	auto ch = Child{ };
 	auto c = daw::observer_ptr( &ch );
