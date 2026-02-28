df40eabb89d24dab3e29f05c2781c4064a49b292
Darrell Wright <beached@users.noreply.github.com>
2026-02-28 14:03:34 -0500

    Enhance `daw_shared_function` with new `reset_storage` virtual function, refactor `shared_function_storage` definitions using macros for reduced duplication, and improve `weak_upgrade` handling.

--- include/daw/daw_shared_function.h
+++ include/daw/daw_shared_function.h
@@ -12,6 +12,8 @@
 #include "daw/traits/daw_copy_cvref_t.h"
 
 #include <atomic>
+#include <concepts>
+#include <optional>
 #include <utility>
 
 namespace daw::sf_impl {
@@ -23,41 +25,57 @@ namespace daw::sf_impl {
 	template<typename T>
 	using param_t = std::conditional_t<std::is_scalar_v<T>, T, T &&>;
 
-#define DAW_SF_STORAGE_BASE_BODY( CALL_QUALS )                             \
-	std::atomic_int m_weak_count = 1;                                        \
-	std::atomic_int m_strong_count = 1;                                      \
-                                                                           \
-public:                                                                    \
-	shared_function_storage_base( ) = default;                               \
-	virtual ~shared_function_storage_base( ) = default;                      \
-                                                                           \
-	virtual DAW_CPP26_CX_ATOMIC R call( Params... )                          \
-	  CALL_QUALS noexcept( IsNoExcept ) = 0;                                 \
-                                                                           \
-	[[nodiscard]] DAW_CPP26_CX_ATOMIC auto *strong_copy( this auto &self ) { \
-		++self.m_strong_count;                                                 \
-		++self.m_weak_count;                                                   \
-		return &self;                                                          \
-	}                                                                        \
-                                                                           \
-	[[nodiscard]] DAW_CPP26_CX_ATOMIC auto *weak_copy( this auto &self ) {   \
-		++self.m_weak_count;                                                   \
-		return &self;                                                          \
-	}                                                                        \
-                                                                           \
-	DAW_CPP26_CX_ATOMIC void strong_dec( ) {                                 \
-		--m_strong_count;                                                      \
-	}                                                                        \
-                                                                           \
-	[[nodiscard]] DAW_CPP26_CX_ATOMIC bool weak_dec( ) {                     \
-		return --m_weak_count == 0;                                            \
+#define DAW_SF_STORAGE_BASE_BODY( CALL_QUALS )                              \
+	std::atomic_int m_weak_count = 1;                                         \
+	std::atomic_int m_strong_count = 1;                                       \
+                                                                            \
+public:                                                                     \
+	shared_function_storage_base( ) = default;                                \
+	DAW_CPP26_CX_ATOMIC virtual ~shared_function_storage_base( ) = default;   \
+	DAW_CPP26_CX_ATOMIC virtual void reset_storage( ) = 0;                    \
+                                                                            \
+	virtual DAW_CPP26_CX_ATOMIC R call( Params... )                           \
+	  CALL_QUALS noexcept( IsNoExcept ) = 0;                                  \
+                                                                            \
+	[[nodiscard]] DAW_CPP26_CX_ATOMIC auto *strong_copy( this auto &self ) {  \
+		++self.m_strong_count;                                                  \
+		++self.m_weak_count;                                                    \
+		return &self;                                                           \
+	}                                                                         \
+                                                                            \
+	[[nodiscard]] DAW_CPP26_CX_ATOMIC auto *weak_upgrade( this auto &self ) { \
+		auto count = self.m_strong_count.load( std::memory_order_relaxed );     \
+		while( count > 0 ) {                                                    \
+			if( self.m_strong_count.compare_exchange_weak(                        \
+			      count,                                                          \
+			      count + 1,                                                      \
+			      std::memory_order_acq_rel,                                      \
+			      std::memory_order_relaxed ) ) {                                 \
+				self.m_weak_count.fetch_add( 1, std::memory_order_relaxed );        \
+				return &self;                                                       \
+			}                                                                     \
+		}                                                                       \
+		return static_cast<decltype( &self )>( nullptr );                       \
+	}                                                                         \
+                                                                            \
+	[[nodiscard]] DAW_CPP26_CX_ATOMIC auto *weak_copy( this auto &self ) {    \
+		++self.m_weak_count;                                                    \
+		return &self;                                                           \
+	}                                                                         \
+                                                                            \
+	[[nodiscard]] DAW_CPP26_CX_ATOMIC bool strong_dec( ) {                    \
+		return --m_strong_count == 0;                                           \
+	}                                                                         \
+                                                                            \
+	[[nodiscard]] DAW_CPP26_CX_ATOMIC bool weak_dec( ) {                      \
+		return --m_weak_count == 0;                                             \
 	}
 
 // One specialization generator
 #define DAW_SF_STORAGE_BASE_SPEC( CVREF_ENUM, CALL_QUALS )                   \
 	template<bool IsNoExcept, typename R, typename... Params>                  \
 	class shared_function_storage_base<CVREF_ENUM, IsNoExcept, R, Params...> { \
-		DAW_SF_STORAGE_BASE_BODY( CALL_QUALS );                                  \
+		DAW_SF_STORAGE_BASE_BODY( CALL_QUALS )                                   \
 	}
 
 	// 6 specializations
@@ -75,102 +93,49 @@ public:                                                                    \
 	         typename... Params>
 	class shared_function_storage;
 
-	template<typename Fn, bool IsNoExcept, typename R, typename... Params>
-	class shared_function_storage<Fn, cvref_t::None, IsNoExcept, R, Params...>
-	  : public shared_function_storage_base<cvref_t::None, IsNoExcept, R,
-	                                        Params...> {
-		DAW_NO_UNIQUE_ADDRESS Fn m_func;
+	template<typename Fn>
+	using function_storage_t =
+	  std::conditional_t<std::is_trivially_destructible_v<Fn>, Fn,
+	                     std::optional<Fn>>;
 
-	public:
-		explicit DAW_CPP26_CX_ATOMIC shared_function_storage( Fn f )
-		  : m_func{ DAW_FWD( f ) } {}
-
-		DAW_CPP26_CX_ATOMIC R
-		call( Params... params ) noexcept( IsNoExcept ) override {
-			return m_func( std::forward<param_t<Params>>( params )... );
-		}
+#define DAW_DETAIL_SHARED_FUNCTION_STORAGE_SPEC( CVRef, CallQuals, InvokeFn ) \
+	template<typename Fn, bool IsNoExcept, typename R, typename... Params>      \
+	class shared_function_storage<Fn, CVRef, IsNoExcept, R, Params...>          \
+	  : public shared_function_storage_base<CVRef, IsNoExcept, R, Params...> {  \
+		using func_t = function_storage_t<Fn>;                                    \
+		static constexpr bool is_optional = not std::same_as<Fn, func_t>;         \
+		DAW_NO_UNIQUE_ADDRESS func_t m_func;                                      \
+                                                                              \
+	public:                                                                     \
+		explicit DAW_CPP26_CX_ATOMIC shared_function_storage( Fn fn )             \
+		  : m_func{ DAW_FWD( fn ) } {}                                            \
+                                                                              \
+		DAW_CPP26_CX_ATOMIC R call( Params... params ) CallQuals                  \
+		  noexcept( IsNoExcept ) override {                                       \
+			if constexpr( is_optional ) {                                           \
+				return ( *InvokeFn )( std::forward<param_t<Params>>( params )... );   \
+			} else {                                                                \
+				return InvokeFn( std::forward<param_t<Params>>( params )... );        \
+			}                                                                       \
+		}                                                                         \
+                                                                              \
+		DAW_CPP26_CX_ATOMIC void reset_storage( ) override {                      \
+			if constexpr( is_optional ) {                                           \
+				m_func.reset( );                                                      \
+			}                                                                       \
+		}                                                                         \
 	};
 
-	template<typename Fn, bool IsNoExcept, typename R, typename... Params>
-	class shared_function_storage<Fn, cvref_t::Const, IsNoExcept, R, Params...>
-	  : public shared_function_storage_base<cvref_t::Const, IsNoExcept, R,
-	                                        Params...> {
-		DAW_NO_UNIQUE_ADDRESS Fn m_func;
+	DAW_DETAIL_SHARED_FUNCTION_STORAGE_SPEC( cvref_t::None, , m_func );
+	DAW_DETAIL_SHARED_FUNCTION_STORAGE_SPEC( cvref_t::Const, const, m_func );
+	DAW_DETAIL_SHARED_FUNCTION_STORAGE_SPEC( cvref_t::Ref, &, m_func );
+	DAW_DETAIL_SHARED_FUNCTION_STORAGE_SPEC( cvref_t::ConstRef, const &, m_func );
+	DAW_DETAIL_SHARED_FUNCTION_STORAGE_SPEC( cvref_t::RefRef, &&,
+	                                         std::move( m_func ) );
+	DAW_DETAIL_SHARED_FUNCTION_STORAGE_SPEC( cvref_t::ConstRefRef, const &&,
+	                                         std::move( m_func ) );
 
-	public:
-		explicit DAW_CPP26_CX_ATOMIC shared_function_storage( Fn fn )
-		  : m_func{ DAW_FWD( fn ) } {}
-
-		DAW_CPP26_CX_ATOMIC R call( Params... params ) const
-		  noexcept( IsNoExcept ) override {
-			return m_func( std::forward<param_t<Params>>( params )... );
-		}
-	};
-
-	template<typename Fn, bool IsNoExcept, typename R, typename... Params>
-	class shared_function_storage<Fn, cvref_t::Ref, IsNoExcept, R, Params...>
-	  : public shared_function_storage_base<cvref_t::Ref, IsNoExcept, R,
-	                                        Params...> {
-		DAW_NO_UNIQUE_ADDRESS Fn m_func;
-
-	public:
-		explicit DAW_CPP26_CX_ATOMIC shared_function_storage( Fn fn )
-		  : m_func{ DAW_FWD( fn ) } {}
-
-		DAW_CPP26_CX_ATOMIC R
-		call( Params... params ) & noexcept( IsNoExcept ) override {
-			return m_func( std::forward<param_t<Params>>( params )... );
-		}
-	};
-
-	template<typename Fn, bool IsNoExcept, typename R, typename... Params>
-	class shared_function_storage<Fn, cvref_t::ConstRef, IsNoExcept, R, Params...>
-	  : public shared_function_storage_base<cvref_t::ConstRef, IsNoExcept, R,
-	                                        Params...> {
-		DAW_NO_UNIQUE_ADDRESS Fn m_func;
-
-	public:
-		explicit DAW_CPP26_CX_ATOMIC shared_function_storage( Fn fn )
-		  : m_func{ DAW_FWD( fn ) } {}
-
-		DAW_CPP26_CX_ATOMIC R
-		call( Params... params ) const & noexcept( IsNoExcept ) override {
-			return m_func( std::forward<param_t<Params>>( params )... );
-		}
-	};
-
-	template<typename Fn, bool IsNoExcept, typename R, typename... Params>
-	class shared_function_storage<Fn, cvref_t::RefRef, IsNoExcept, R, Params...>
-	  : public shared_function_storage_base<cvref_t::RefRef, IsNoExcept, R,
-	                                        Params...> {
-		DAW_NO_UNIQUE_ADDRESS Fn m_func;
-
-	public:
-		explicit DAW_CPP26_CX_ATOMIC shared_function_storage( Fn fn )
-		  : m_func{ DAW_FWD( fn ) } {}
-
-		DAW_CPP26_CX_ATOMIC R
-		call( Params... params ) && noexcept( IsNoExcept ) override {
-			return std::move( m_func )( std::forward<param_t<Params>>( params )... );
-		}
-	};
-
-	template<typename Fn, bool IsNoExcept, typename R, typename... Params>
-	class shared_function_storage<Fn, cvref_t::ConstRefRef, IsNoExcept, R,
-	                              Params...>
-	  : public shared_function_storage_base<cvref_t::ConstRefRef, IsNoExcept, R,
-	                                        Params...> {
-		DAW_NO_UNIQUE_ADDRESS Fn m_func;
-
-	public:
-		explicit DAW_CPP26_CX_ATOMIC shared_function_storage( Fn fn )
-		  : m_func{ DAW_FWD( fn ) } {}
-
-		DAW_CPP26_CX_ATOMIC R
-		call( Params... params ) const && noexcept( IsNoExcept ) override {
-			return std::move( m_func )( std::forward<param_t<Params>>( params )... );
-		}
-	};
+#undef DAW_DETAIL_SHARED_FUNCTION_STORAGE_SPEC
 
 	template<cvref_t CVRef, bool IsNoExcept, typename R, typename... Params>
 	DAW_CPP26_CX_ATOMIC void
@@ -191,7 +156,9 @@ public:                                                                    \
 		if( not p ) {
 			return;
 		}
-		p->strong_dec( );
+		if( p->strong_dec( ) ) {
+			p->reset_storage( );
+		}
 		weak_release( p );
 	}
 
@@ -222,10 +189,10 @@ public:                                                                    \
 
 		storage_t *m_storage = nullptr;
 
-	public:
 		explicit DAW_CPP26_CX_ATOMIC weak_shared_function_base( storage_t *storage )
-		  : m_storage( storage ? storage->weak_copy( ) : nullptr ) {}
+		  : m_storage( storage ) {}
 
+	public:
 		DAW_CPP26_CX_ATOMIC
 		weak_shared_function_base( weak_shared_function_base const &other )
 		  : m_storage( other.m_storage ? other.m_storage->weak_copy( ) : nullptr ) {
@@ -268,7 +235,7 @@ public:                                                                    \
 		storage_t *m_storage = nullptr;
 
 		DAW_CPP26_CX_ATOMIC shared_function_base( storage_t *storage )
-		  : m_storage( storage ? storage->strong_copy( ) : nullptr ) {}
+		  : m_storage( storage ) {}
 
 	public:
 		template<typename Fn>
@@ -377,11 +344,11 @@ namespace daw {
 
 #undef DAW_DETAIL_SHARED_FUNCTION_SPEC
 
-#define DAW_DETAIL_SHARED_FUNCTION_GET_WEAK_DEF( Signature )   \
-	template<typename R, typename... Params>                     \
-	DAW_CPP26_CX_ATOMIC weak_shared_function<Signature>          \
-	shared_function<Signature>::get_weak( ) {                    \
-		return weak_shared_function<Signature>( this->m_storage ); \
+#define DAW_DETAIL_SHARED_FUNCTION_GET_WEAK_DEF( Signature ) \
+	template<typename R, typename... Params>                   \
+	DAW_CPP26_CX_ATOMIC weak_shared_function<Signature>        \
+	shared_function<Signature>::get_weak( ) {                  \
+		return weak_shared_function<Signature>( *this );         \
 	}
 
 	DAW_DETAIL_SHARED_FUNCTION_GET_WEAK_DEF( R( Params... ) );
@@ -411,11 +378,13 @@ namespace daw {
                                                                              \
 		explicit DAW_CPP26_CX_ATOMIC                                             \
 		weak_shared_function( shared_function<Signature> const &s )              \
-		  : base_t( s.m_storage ) {}                                             \
+		  : base_t( s.m_storage ? s.m_storage->weak_copy( ) : nullptr ) {}       \
                                                                              \
 	public:                                                                    \
 		[[nodiscard]] DAW_CPP26_CX_ATOMIC shared_function<Signature> lock( ) {   \
-			return shared_function<Signature>( this->m_storage );                  \
+			auto *storage =                                                        \
+			  this->m_storage ? this->m_storage->weak_upgrade( ) : nullptr;        \
+			return shared_function<Signature>( storage );                          \
 		}                                                                        \
 	}
 
