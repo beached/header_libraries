6bd7db0de1cb36ee12a9ab50f1bd8ed2fe6d7aeb
Darrell Wright <beached@users.noreply.github.com>
2026-09-13 21:15:23 -0400

    Simplify `daw::as` implementation by removing unused traits and macros.

--- include/daw/daw_as.h
+++ include/daw/daw_as.h
@@ -9,25 +9,13 @@
 #pragma once
 
 #include "daw/daw_attributes.h"
-#include "daw/daw_cpp_feature_check.h"
-#include "daw/daw_enable_requires.h"
 #include "daw/daw_move.h"
-#include "daw/impl/daw_make_trait.h"
-
-#include <type_traits>
-#include <utility>
 
 namespace daw {
-	namespace as_impl {
-		DAW_MAKE_REQ_TRAIT2( explicitly_convertible_to_v,
-		                     static_cast<T>( std::declval<U>( ) ) );
-	}
-
-	template<typename To, typename From DAW_ENABLEIF(
-	                        as_impl::explicitly_convertible_to_v<To, From> )>
-	DAW_REQUIRES( as_impl::explicitly_convertible_to_v<To, From> )
-	[[nodiscard]] DAW_ATTRIB_INSTRINSIC DAW_ATTRIB_INLINE constexpr To as(
-	  From &&from ) noexcept( noexcept( static_cast<To>( DAW_FWD( from ) ) ) ) {
+	template<typename To, typename From>
+	[[nodiscard]] DAW_ATTRIB_INSTRINSIC DAW_ATTRIB_INLINE constexpr auto
+	as( From &&from ) noexcept( noexcept( static_cast<To>( DAW_FWD( from ) ) ) )
+	  -> decltype( static_cast<To>( DAW_FWD( from ) ) ) {
 		return static_cast<To>( DAW_FWD( from ) );
 	}
 } // namespace daw
