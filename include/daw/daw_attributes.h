6c606975a63eae82b2b2ab8715fd547554d79f91
Darrell Wright <beached@users.noreply.github.com>
2026-09-21 01:02:40 -0400

    Remove `DAW_ATTRIB_INSTRINSIC` macro usage

--- include/daw/daw_attributes.h
+++ include/daw/daw_attributes.h
@@ -159,7 +159,7 @@
 #endif
 
 #if DAW_HAS_MSVC_VER_GTE( 1935 ) and DAW_HAS_ATTRIBUTE( msvc::intrinsic )
-#define  [[msvc::intrinsic]]
+#define DAW_ATTRIB_INTRINSIC [[msvc::intrinsic]]
 #else
 #define
 #endif
