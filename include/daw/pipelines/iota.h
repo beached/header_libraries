91197447045826320e9283df818949ff73e84b82
Darrell Wright <beached@users.noreply.github.com>
2026-09-20 23:14:09 -0400

    Add countdown tests, implement `countdown_iterator` and `countdown_view`, and enhance `iota_iterator` arithmetic.

--- include/daw/pipelines/iota.h
+++ include/daw/pipelines/iota.h
@@ -12,6 +12,7 @@
 #include "daw/daw_as.h"
 #include "daw/daw_concepts.h"
 #include "daw/daw_ensure.h"
+#include "daw/daw_int_cmp.h"
 #include "daw/daw_iterator_traits.h"
 #include "daw/iterator/daw_arrow_proxy.h"
 #include "daw/pipelines/range_base.h"
@@ -39,43 +40,41 @@ namespace daw::pipelines {
 
 		explicit constexpr iota_iterator(
 		  daw::explicitly_convertible_to<T> auto &&value )
-		  : m_value( static_cast<T>( DAW_FWD( value ) ) ) {}
+		  : m_value( as<T>( DAW_FWD( value ) ) ) {}
 
 		[[nodiscard]] DAW_ATTRIB_INLINE constexpr reference operator*( ) const {
 			return m_value;
 		}
 
 		constexpr iota_iterator &operator++( ) {
-			return *this += 1;
+			return *this += value_type{ 1 };
 		}
 
 		[[nodiscard]] constexpr iota_iterator operator++( int ) {
 			iota_iterator result = *this;
-			++m_value;
+			*this += value_type{ 1 };
 			return result;
 		}
 
 		constexpr iota_iterator &operator--( ) {
-			return *this -= 1;
+			return *this -= value_type{ 1 };
 		}
 
 		[[nodiscard]] constexpr iota_iterator operator--( int ) {
 			iota_iterator result = *this;
-			--m_value;
+			*this -= value_type{ 1 };
 			return result;
 		}
 
 		DAW_ATTRIB_INLINE constexpr iota_iterator &operator+=( difference_type n ) {
 			// size_type is the unsigned type as wide as difference_type.  Adding in
 			// it wraps, where a signed sum could overflow for a large step
-			m_value = static_cast<T>( static_cast<size_type>( m_value ) +
-			                          static_cast<size_type>( n ) );
+			m_value = as<T>( as<size_type>( m_value ) + as<size_type>( n ) );
 			return *this;
 		}
 
 		DAW_ATTRIB_INLINE constexpr iota_iterator &operator-=( difference_type n ) {
-			m_value = static_cast<T>( static_cast<size_type>( m_value ) -
-			                          static_cast<size_type>( n ) );
+			m_value = as<T>( as<size_type>( m_value ) - as<size_type>( n ) );
 			return *this;
 		}
 
@@ -97,20 +96,14 @@ namespace daw::pipelines {
 			return lhs;
 		}
 
-		[[nodiscard]] constexpr friend iota_iterator
-		operator-( difference_type n, iota_iterator rhs ) noexcept {
-			rhs -= n;
-			return rhs;
-		}
-
 		[[nodiscard]] constexpr difference_type
 		operator-( iota_iterator const &i ) const {
 			return as<difference_type>( m_value ) - as<difference_type>( i.m_value );
 		}
 
-		[[nodiscard]] constexpr reference
+		DAW_ATTRIB_FLATTEN [[nodiscard]] constexpr reference
 		operator[]( difference_type n ) const noexcept {
-			return as<value_type>( static_cast<difference_type>( m_value ) + n );
+			return *( *this + n );
 		}
 
 		[[nodiscard]] constexpr bool
@@ -140,15 +133,15 @@ namespace daw::pipelines {
 
 		explicit constexpr iota_view(
 		  daw::explicitly_convertible_to<T> auto &&last )
-		  : m_last{ static_cast<T>( DAW_FWD( last ) ) } {
+		  : m_last{ as<T>( DAW_FWD( last ) ) } {
 			daw_dbg_ensure( m_first <= m_last );
 		}
 
 		explicit constexpr iota_view(
 		  daw::explicitly_convertible_to<T> auto &&first,
 		  daw::explicitly_convertible_to<T> auto &&last )
-		  : m_first{ static_cast<T>( DAW_FWD( first ) ) }
-		  , m_last{ static_cast<T>( DAW_FWD( last ) ) } {
+		  : m_first{ as<T>( DAW_FWD( first ) ) }
+		  , m_last{ as<T>( DAW_FWD( last ) ) } {
 			daw_dbg_ensure( m_first <= m_last );
 		}
 
@@ -164,12 +157,9 @@ namespace daw::pipelines {
 			return m_first == m_last;
 		}
 
-		[[nodiscard]] constexpr value_type
+		DAW_ATTRIB_FLATTEN [[nodiscard]] constexpr value_type
 		operator[]( difference_type n ) const noexcept {
-			daw_dbg_ensure( not empty( ) );
-			daw_dbg_ensure( std::cmp_less_equal( m_first, n ) );
-			daw_dbg_ensure( std::cmp_less( n, m_last ) );
-			return as<value_type>( static_cast<difference_type>( *begin( ) ) + n );
+			return begin( )[n];
 		}
 
 		[[nodiscard]] constexpr bool operator==( iota_view const &rhs ) const {
@@ -177,10 +167,159 @@ namespace daw::pipelines {
 		}
 	};
 	template<typename T>
-	iota_view( T ) -> iota_view<std::size_t>;
+	iota_view( T ) -> iota_view<T>;
 
 	template<typename T>
-	iota_view( T, T ) -> iota_view<std::size_t>;
+	iota_view( T, T ) -> iota_view<T>;
+
+	template<typename T>
+	struct countdown_iterator {
+		using iterator_category = std::random_access_iterator_tag;
+		using value_type = T;
+		using reference = value_type;
+		using const_reference = value_type;
+		using difference_type = daw::next_wider_t<daw::make_signed_t<T>>;
+		using size_type = daw::next_wider_t<daw::make_unsigned_t<T>>;
+
+	private:
+		value_type m_from = T{ };
+
+	public:
+		countdown_iterator( ) = default;
+
+		explicit constexpr countdown_iterator(
+		  daw::explicitly_convertible_to<value_type> auto &&from )
+		  : m_from( as<value_type>( DAW_FWD( from ) ) ) {}
+
+		[[nodiscard]] DAW_ATTRIB_INLINE constexpr reference operator*( ) const {
+			return m_from;
+		}
+
+		constexpr countdown_iterator &operator++( ) {
+			return *this += value_type{ 1 };
+		}
+
+		[[nodiscard]] constexpr countdown_iterator operator++( int ) {
+			countdown_iterator result = *this;
+			*this += value_type{ 1 };
+			return result;
+		}
+
+		constexpr countdown_iterator &operator--( ) {
+			return *this -= value_type{ 1 };
+		}
+
+		[[nodiscard]] constexpr countdown_iterator operator--( int ) {
+			countdown_iterator result = *this;
+			*this -= value_type{ 1 };
+			return result;
+		}
+
+		DAW_ATTRIB_INLINE constexpr countdown_iterator &
+		operator+=( difference_type n ) {
+			m_from = as<value_type>( as<size_type>( m_from ) - as<size_type>( n ) );
+			return *this;
+		}
+
+		DAW_ATTRIB_INLINE constexpr countdown_iterator &
+		operator-=( difference_type n ) {
+			m_from = as<value_type>( as<size_type>( m_from ) + as<size_type>( n ) );
+			return *this;
+		}
+
+		[[nodiscard]] constexpr friend countdown_iterator
+		operator+( countdown_iterator lhs, difference_type n ) noexcept {
+			lhs += n;
+			return lhs;
+		}
+
+		[[nodiscard]] constexpr friend countdown_iterator
+		operator+( difference_type n, countdown_iterator rhs ) noexcept {
+			rhs += n;
+			return rhs;
+		}
+
+		[[nodiscard]] constexpr friend countdown_iterator
+		operator-( countdown_iterator lhs, difference_type n ) noexcept {
+			lhs -= n;
+			return lhs;
+		}
+
+		[[nodiscard]] constexpr difference_type
+		operator-( countdown_iterator const &i ) const {
+			// the value falls as the iterator advances
+			return as<difference_type>( i.m_from ) - as<difference_type>( m_from );
+		}
+
+		DAW_ATTRIB_FLATTEN [[nodiscard]] constexpr reference
+		operator[]( difference_type n ) const noexcept {
+			return *( *this + n );
+		}
+
+		[[nodiscard]] constexpr bool
+		operator==( countdown_iterator const &rhs ) const = default;
+
+		// clang-format off
+		[[nodiscard]] friend constexpr auto operator<=>
+		  ( countdown_iterator const &lhs, countdown_iterator const &rhs ) {
+			return rhs.m_from <=> lhs.m_from;
+		}
+		// clang-format on
+	};
+	template<typename T>
+	struct countdown_view {
+		using iterator = countdown_iterator<T>;
+		using difference_type = daw::iter_difference_t<iterator>;
+		using value_type = daw::iter_value_t<iterator>;
+		using size_type = daw::iter_size_t<iterator>;
+
+	private:
+		T m_from = max_value<T>;
+		T m_to = T{ };
+
+	public:
+		explicit countdown_view( ) = default;
+
+		explicit constexpr countdown_view(
+		  daw::explicitly_convertible_to<T> auto &&from )
+		  : m_from{ as<T>( DAW_FWD( from ) ) } {
+			daw_dbg_ensure( m_from >= m_to );
+		}
+
+		explicit constexpr countdown_view(
+		  daw::explicitly_convertible_to<T> auto &&first,
+		  daw::explicitly_convertible_to<T> auto &&last )
+		  : m_from{ as<T>( DAW_FWD( first ) ) }
+		  , m_to{ as<T>( DAW_FWD( last ) ) } {
+			daw_dbg_ensure( m_from >= m_to );
+		}
+
+		[[nodiscard]] constexpr iterator begin( ) const {
+			return iterator{ m_from };
+		}
+
+		[[nodiscard]] constexpr iterator end( ) const {
+			return iterator{ m_to };
+		}
+
+		[[nodiscard]] constexpr bool empty( ) const {
+			return m_from == m_to;
+		}
+
+		DAW_ATTRIB_FLATTEN [[nodiscard]] constexpr value_type
+		operator[]( difference_type n ) const noexcept {
+			return begin( )[n];
+		}
+
+		[[nodiscard]] constexpr bool operator==( countdown_view const &rhs ) const {
+			return m_from == rhs.m_from and m_to == rhs.m_to;
+		}
+	};
+	template<typename T>
+	countdown_view( T ) -> countdown_view<T>;
+
+	template<typename T>
+	countdown_view( T, T ) -> countdown_view<T>;
 
 	inline constexpr auto ToIota = []<Integer I>( I last ) {
 		return iota_view<I>{ I{ }, last };
